548400cd5650706820ee0dec1bb86cfff616c3b4
yufeng <fengyu8299@gmail.com>
2013-07-10 15:47:52 -0400

    1.Modify the method in bundle.java to support unknown field; 2. Rollback Saswat's change on static extras field.

--- models/src/android/content/Intent.java
+++ models/src/android/content/Intent.java
@@ -3,7 +3,7 @@ package android.content;
 class Intent
 {
 	//add by yu.
-	private static android.os.Bundle extras = new android.os.Bundle();
+	private android.os.Bundle extras = new android.os.Bundle();
 	
 	public  Intent(java.lang.String action, android.net.Uri uri) 
 	{ 
