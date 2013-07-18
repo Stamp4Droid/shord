519535848d88d025ff26ac93af64d0bdab271fe3
brycecr <brycecr@gmail.com>
2013-07-18 13:29:54 -0700

    merging stashed chagnes. Towards solvergen

--- main/src/stamp/reporting/SrcSinkFlowViz.java
+++ main/src/stamp/reporting/SrcSinkFlowViz.java
@@ -24,6 +24,8 @@ public class SrcSinkFlowViz extends XMLVizReport
 
     public void generate()
 	{
+		/*
+		//This code is for Osbert's JCFL solver stuff. Will fail if JCFL doesn't come through.
 		try {
 			BufferedReader brdr = new BufferedReader(new FileReader("cfl/Src2SinkFiles.out"));
 
@@ -63,6 +65,36 @@ public class SrcSinkFlowViz extends XMLVizReport
 			}
 		} catch (Exception e) {
 			System.err.println("Failed to open cfl/Src2SinkFiles.out");
+		}
+		*/
+
+		//This code should work with Manolis' solvergen stuff
+		try {
+			BufferedReader br = new BufferedReader(new FileReader("solvergen/solvergenpaths.out"));
+
+			while ((line = br.readLine()) != null) {
+				Category c = null;
+				if (line.length() == 0) {
+					//Empty line; delim between flows
+					c = null;
+
+				} else if (line.charAt(0) == '$') {
+					//should indicate first line of new flow, format $SRC-->!SINK
+					c = makeOrGetPkgCat(new SootClass(line));
+
+				} else {
+					/*
+					--label2Ref-> [$r13 = virtualinvoke $r12.<android.telephony.TelephonyManager: java.lang.String getDeviceId()>()@<stamp.stanford.malware.Malware: void onCreate(android.os.Bundle)>,virtualinvoke $r1.<android.app.Activity: void onCreate(android.os.Bundle)>(null)@<android.app.Activity$1: void run()>]:return@<android.telephony.TelephonyManager: java.lang.String getDeviceId()>
+					*/
+					System.out.p
+					//edge of the path
+				}
+
+			}
+
+		} catch (Exception e) {
+			System.err.println("Error reading solvergen/solvergenpaths.out");
+			e.printStackTrace();
 		}
 	}
 }
