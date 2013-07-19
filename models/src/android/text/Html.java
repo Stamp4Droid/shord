6e814ce8844fdc233ee33fbfd9c4d605283fdc6e
Saswat Anand <saswat@stamp.(none)>
2013-07-19 02:25:51 -0700

    models

--- models/src/android/text/Html.java
+++ models/src/android/text/Html.java
@@ -11,5 +11,15 @@ class Html {
     public static android.text.Spanned fromHtml(java.lang.String source, android.text.Html.ImageGetter imageGetter, android.text.Html.TagHandler tagHandler) {
         return new android.text.SpannableString(source);
     }
+
+    @STAMP(flows = { @Flow(from = "text", to = "@return") })
+    public static java.lang.String toHtml(android.text.Spanned text) {
+		return new String();
+    }
+
+    @STAMP(flows = { @Flow(from = "text", to = "@return") })
+    public static java.lang.String escapeHtml(java.lang.CharSequence text) {
+		return new String();
+    }
 }
 
