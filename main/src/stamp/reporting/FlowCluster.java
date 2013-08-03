7b9c711f7792dd8232db125cd573165578eff13d
Saswat Anand <saswat@stamp.(none)>
2013-08-03 02:42:24 -0700

    fixed memory problem of clustering

--- main/src/stamp/reporting/FlowCluster.java
+++ main/src/stamp/reporting/FlowCluster.java
@@ -6,6 +6,7 @@ import java.util.Map;
 import java.util.HashMap;
 import java.util.Collection;
 import java.util.List;
+import java.util.ArrayList;
 
 import shord.project.ClassicProject;
 import shord.project.analyses.ProgramRel;
@@ -14,23 +15,26 @@ import shord.analyses.VarNode;
 
 import stamp.util.Partition;
 
+import chord.bddbddb.Rel.RelView;
 import chord.util.tuple.object.Trio;
 import chord.util.tuple.object.Pair;
+
+import soot.util.BitVector;
+import soot.util.MapNumberer;
 
 /*
  * @author Saswat Anand
 **/
 public class FlowCluster
 {
-	private static final Map<Pair<String,Ctxt>,Set<Pair<VarNode,Ctxt>>> labelToBucket = new HashMap();
-	private static final Map<Pair<Pair<String,Ctxt>,Pair<String,Ctxt>>,Set<Pair<VarNode,Ctxt>>> flowToBucket = new HashMap();
+	private static final Map<Pair<Pair<String,Ctxt>,Pair<String,Ctxt>>,BitVector> flowToBucket = new HashMap();
+	private static final Map<Pair<String,Ctxt>,List<Pair<Pair<String,Ctxt>,Pair<String,Ctxt>>>> labelToFlows = new HashMap();
 	private static final double threshhold = Double.parseDouble(System.getProperty("stamp.flowcluster.threshhold", "0.9"));
 
 	static void cluster()
 	{
 		System.out.println("Begin clustering");
-		fillLabelBuckets();
-		fillFlowBuckets();
+		fillBuckets();
 		
 		Object[] flows = flowToBucket.keySet().toArray();
 		int numFlows = flows.length;
@@ -58,79 +62,99 @@ public class FlowCluster
 		System.out.println("End clustering");
 	}
 
-	static double ochiai(Set<Pair<VarNode,Ctxt>> set1, Set<Pair<VarNode,Ctxt>> set2)
+	static double ochiai(BitVector set1, BitVector set2)
 	{
-		Set<Pair<VarNode,Ctxt>> intersection = new HashSet<Pair<VarNode,Ctxt>>(set1);
-		intersection.retainAll(set2);
+		BitVector intersection = new BitVector(set1);
+		intersection.and(set2);
 		
-		double index = intersection.size() / Math.sqrt(set1.size()*set2.size());
+		double index = intersection.cardinality() / Math.sqrt(set1.cardinality()*set2.cardinality());
 		return index;
 	}
 
-	static void fillFlowBuckets()
+	private static void initBuckets(int numVars)
 	{
 		final ProgramRel relCtxtFlows = (ProgramRel)ClassicProject.g().getTrgt("flow");
 		relCtxtFlows.load();
 
 		Iterable<Pair<Pair<String,Ctxt>,Pair<String,Ctxt>>> res = relCtxtFlows.getAry2ValTuples();
 		int count = 0;
-		for(Pair<Pair<String,Ctxt>,Pair<String,Ctxt>> pair : res) {
-			Pair<String,Ctxt> srcLabel = pair.val0;
-			Pair<String,Ctxt> sinkLabel = pair.val1;
+		for(Pair<Pair<String,Ctxt>,Pair<String,Ctxt>> flow : res) {
+			Pair<String,Ctxt> srcLabel = flow.val0;
+			Pair<String,Ctxt> sinkLabel = flow.val1;
 			
-			Pair<Pair<String,Ctxt>,Pair<String,Ctxt>> flow = new Pair<Pair<String,Ctxt>,Pair<String,Ctxt>>(srcLabel, sinkLabel);
-			
-			Set<Pair<VarNode,Ctxt>> bucket = flowToBucket.get(flow);
+			BitVector bucket = flowToBucket.get(flow);
 			if(bucket == null){
-				bucket = new HashSet();
+				bucket = new BitVector(numVars);
 				flowToBucket.put(flow, bucket);
 			}
-			
-			for(Pair<VarNode,Ctxt> v : getBucketForLabel(srcLabel)){
-				bucket.add(v);
+
+			List<Pair<Pair<String,Ctxt>,Pair<String,Ctxt>>> flows = labelToFlows.get(srcLabel);
+			if(flows == null){
+				flows = new ArrayList();
+				labelToFlows.put(srcLabel, flows);
 			}
-			for(Pair<VarNode,Ctxt> v : getBucketForLabel(sinkLabel)){
-				bucket.add(v);
-			}			
+			flows.add(flow);
+
+			flows = labelToFlows.get(sinkLabel);
+			if(flows == null){
+				flows = new ArrayList();
+				labelToFlows.put(sinkLabel, flows);
+			}
+			flows.add(flow);			
 		}
+		relCtxtFlows.close();
 	}
 
-	static void fillLabelBuckets()
+	private static void fillBuckets()
 	{
 		final ProgramRel relRef = (ProgramRel) ClassicProject.g().getTrgt("labelRef");
 		relRef.load();
-		Iterable<Trio<Ctxt,VarNode,Pair<String,Ctxt>>> res1 = relRef.getAry3ValTuples();
-		fillBuckets(res1);
-		relRef.close();
 
 		final ProgramRel relPrim = (ProgramRel) ClassicProject.g().getTrgt("labelPrim");
 		relPrim.load();
-		Iterable<Trio<Ctxt,VarNode,Pair<String,Ctxt>>> res2 = relPrim.getAry3ValTuples();
-		fillBuckets(res2);
+
+		MapNumberer taintedVarNumberer = new MapNumberer();
+		
+		populateDomain(taintedVarNumberer, relRef);
+		populateDomain(taintedVarNumberer, relPrim);
+		
+		initBuckets(taintedVarNumberer.size());
+
+		fillBuckets(taintedVarNumberer, relRef);
+		fillBuckets(taintedVarNumberer, relPrim);
+
+		relRef.close();
 		relPrim.close();
 	}
-
-	private static void fillBuckets(Iterable<Trio<Ctxt,VarNode,Pair<String,Ctxt>>> iter)
+	
+	private static void populateDomain(MapNumberer varNumberer, ProgramRel rel)
 	{
+		RelView taintedVars = rel.getView();
+		taintedVars.delete(2); //drop labels
+
+		Iterable<Pair<Ctxt,VarNode>> iter = taintedVars.getAry2ValTuples();
+		for(Pair<Ctxt,VarNode> pair : iter){
+			varNumberer.add(pair);
+		}
+	}
+
+	private static void fillBuckets(MapNumberer taintedVarNumberer, ProgramRel rel)
+	{
+		Iterable<Trio<Ctxt,VarNode,Pair<String,Ctxt>>> iter = rel.getAry3ValTuples();
 		for(Trio<Ctxt,VarNode,Pair<String,Ctxt>> trio : iter) {
-			Ctxt varCtxt = trio.val0;
+			Ctxt ctxt = trio.val0;
 			VarNode var = trio.val1;
 			Pair<String,Ctxt> label = trio.val2;
-			
-			Set<Pair<VarNode,Ctxt>> bucket = getBucketForLabel(label);
-			bucket.add(new Pair<VarNode,Ctxt>(var, varCtxt));
+
+			List<Pair<Pair<String,Ctxt>,Pair<String,Ctxt>>> flows = labelToFlows.get(label);
+			if(flows == null)
+				continue;
+			Pair<Ctxt,VarNode> ctxtVar = new Pair(ctxt,var);
+			int ctxtVarIndex = (int) taintedVarNumberer.get(ctxtVar);
+			for(Pair<Pair<String,Ctxt>,Pair<String,Ctxt>> flow : flows){
+				BitVector bucket = flowToBucket.get(flow);
+				bucket.set(ctxtVarIndex);
+			}
 		}
 	}
-	
-	private static Set<Pair<VarNode,Ctxt>> getBucketForLabel(Pair<String,Ctxt> label)
-	{
-		Set<Pair<VarNode,Ctxt>> bucket = labelToBucket.get(label);
-		if(bucket == null){
-			bucket = new HashSet();
-			labelToBucket.put(label, bucket);
-		}
-		return bucket;
-	}
-		
-		
 }
