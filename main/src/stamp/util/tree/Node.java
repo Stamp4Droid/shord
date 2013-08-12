4c3a8dbbc95442ebc3e3fef7ebb11bbc60d12746
brycecr <brycecr@gmail.com>
2013-08-12 12:02:27 -0700

    Changes to simple Tree to support vizrewrite

--- main/src/stamp/util/tree/Node.java
+++ main/src/stamp/util/tree/Node.java
@@ -1,19 +1,3 @@
-/*
- * Copyright 2013 Bryce Cronkite-Ratcliff.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package tree;
 
 import java.util.ArrayList;
@@ -77,6 +61,10 @@ public class Node<T> {
 				+ "parent to Node with non-null parent.");
 		}
 	}
+
+        public T getData() {
+            return data;
+        }
 
 	private static class InvalidNodeOpException extends Exception {
 
--- main/src/stamp/util/tree/Tree.java
+++ main/src/stamp/util/tree/Tree.java
@@ -1,19 +1,3 @@
-/*
- * Copyright 2013 Bryce Cronkite-Ratcliff.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package tree;
 
 /**
@@ -31,6 +15,10 @@ public class Tree<T> {
 	public Node<T> getRoot() {
 		return root;
 	}
+
+        public boolean isRoot(T data) {
+            return root.getData().equals(data);
+        }
 
 	public Node<T> getParent(Node<T> child) {
 		if (child == null) {
