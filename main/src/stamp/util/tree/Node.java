09297b6af8a42edba767b58c8e843aa49a0b13a1
brycecr <brycecr@gmail.com>
2013-08-12 13:36:42 -0700

    refactoring of tree code

--- main/src/stamp/util/tree/Node.java
+++ main/src/stamp/util/tree/Node.java
@@ -8,68 +8,68 @@ import java.util.ArrayList;
  */
 public class Node<T> {
 
-	protected ArrayList<Node<T>> children = new ArrayList<Node<T>>();
-	protected Node<T> parent = null;
-	protected T data;
+    protected ArrayList<Node<T>> children = new ArrayList<Node<T>>();
+    protected Node<T> parent = null;
+    protected T data;
 
-	public Node(T id) {
-		data = id;
-	}
+    public Node(T id) {
+        data = id;
+    }
 
-	public Node(T id, ArrayList<Node<T>> initchildren) {
-		children = initchildren;
-		data = id;
-	}
+    public Node(T id, ArrayList<Node<T>> initchildren) {
+        children = initchildren;
+        data = id;
+    }
 
-	public Node(T id, Node<T> initparent) {
-		parent = initparent;
-		data = id;
-	}
+    public Node(T id, Node<T> initparent) {
+        parent = initparent;
+        data = id;
+    }
 
-	public Node(T id, Node<T> initparent, ArrayList<Node<T>> initchildren) {
-		parent = initparent;
-		children = initchildren;
-		data = id;
-	}
+    public Node(T id, Node<T> initparent, ArrayList<Node<T>> initchildren) {
+        parent = initparent;
+        children = initchildren;
+        data = id;
+    }
 
-	/**
-	 * 
-	 * @return parent node of this node. May be null.
-	 */
-	public Node<T> getParent() {
-		return parent;
-	}
+    /**
+     * 
+     * @return parent node of this node. May be null.
+     */
+    public Node<T> getParent() {
+        return parent;
+    }
 
-	/**
-	 * @return ArrayList of children of this node
-	 */
-	public ArrayList<Node<T>> getChildren() {
-		assert children != null;
-		return children;
-	}
+    /**
+     * @return ArrayList of children of this node
+     */
+    public ArrayList<Node<T>> getChildren() {
+        assert children != null;
+        return children;
+    }
 
-	public Node<T> addChild(Node<T> newchild) {
-		children.add(newchild);
-		return newchild;
-	}
+    public Node<T> addChild(Node<T> newchild) {
+        children.add(newchild);
+        return newchild;
+    }
 
-	public void addParent(Node<T> newparent) throws InvalidNodeOpException {
-		if (parent == null) {
-			parent = newparent;
-		} else {
-			throw new InvalidNodeOpException("Tree: Adding new "
-				+ "parent to Node with non-null parent.");
-		}
-	}
-
-        public T getData() {
-            return data;
+    public void addParent(Node<T> newparent) throws InvalidNodeOpException {
+        if (parent == null) {
+            parent = newparent;
+        } else {
+            throw new InvalidNodeOpException("Tree: Adding new "
+                    + "parent to Node with non-null parent.");
         }
+    }
 
-	private static class InvalidNodeOpException extends Exception {
+    public T getData() {
+        return data;
+    }
 
-		public InvalidNodeOpException(String str) {
-			super(str);
-		}
-	}
+    private static class InvalidNodeOpException extends Exception {
+
+        public InvalidNodeOpException(String str) {
+            super(str);
+        }
+    }
 }
--- main/src/stamp/util/tree/Tree.java
+++ main/src/stamp/util/tree/Tree.java
@@ -6,40 +6,55 @@ package tree;
  */
 public class Tree<T> {
 
-	protected Node<T> root;
+    protected Node<T> root;
 
-	public Tree(T treename) {
-		root = new Node<T>(treename);
-	}
+    public Tree(T treename) {
+        root = new Node<T>(treename);
+    }
 
-	public Node<T> getRoot() {
-		return root;
-	}
+    public Node<T> getRoot() {
+        return root;
+    }
 
-        public boolean isRoot(T data) {
-            return root.getData().equals(data);
+    public boolean isRoot(T data) {
+        return root.getData().equals(data);
+    }
+
+    public Node<T> getParent(Node<T> child) {
+        if (child == null) {
+            return null;
         }
+        //if (isInTree(child)) { //TODO: implement this part and throw
+        //			 //exception on fail
+        Node<T> parent = child.getParent();
+        if (parent == null) {
+            return getRoot();
+        }
+        //} else {
+        //	return null;
+        //}
+        return null;
+    }
 
-	public Node<T> getParent(Node<T> child) {
-		if (child == null) {
-			return null;
-		}
-		//if (isInTree(child)) { //TODO: implement this part and throw
-		//			 //exception on fail
-			Node<T> parent = child.getParent();
-			if (parent == null) {
-				return getRoot();
-			}
-		//} else {
-		//	return null;
-		//}
-		return null;
-	}
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
 
-	/**
-	 * @param args the command line arguments
-	 */
-	public static void main(String[] args) {
-		// TODO code application logic here
-	}
+    class TreeIterator<T> implements Iterator {
+    
+        @implement
+        public boolean hasNext() {
+
+        }
+        
+        @implement
+        public T next() {
+
+        }
+        
+        // Does not implement remove
+    }
 }
