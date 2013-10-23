b36d21f91df3638a59dbbae0c14bdb13d6eef4e8
brycecr <brycecr@gmail.com>
2013-10-22 23:52:19 -0700

    now reading in src sink lists from xml files on page load instead of hardcoded. Not in actual databases yet (maybe wont be for a bit...).

---
+++ web/src/stamp/submitting/PolicyServlet.java
@@ -0,0 +1,63 @@
+package stamp.submitting;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.ServletException;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.logging.Logger;
+
+@WebServlet(name="PolicyServlet", urlPatterns={"/policyServlet"})
+public class PolicyServlet extends HttpServlet 
+{
+
+	private static Logger logger = Logger.getLogger(PolicyServlet.class.getName());
+
+	public PolicyServlet() 
+	{
+		super();
+	}
+
+	/**
+	 * GET requests expected include:
+	 *   > Request for list of known srcs or sinks
+	 *  
+	 */
+	protected void doGet (HttpServletRequest request,
+		HttpServletResponse response) 
+	throws ServletException, IOException
+	{
+		if (request.getParameter("annot") != null) {
+			String filename = "";
+			if (request.getParameter("annot").equals("Sources")) {
+				filename = "../scripts/srcClass.xml";
+			} else if (request.getParameter("annot").equals("Sinks")) {
+				filename = "../scripts/sinkClass.xml";
+			} else {
+				return;
+			}
+			FileInputStream in = new FileInputStream(filename);
+			OutputStream out = response.getOutputStream();
+			byte[] buffer = new byte[4096];
+			int length;
+			while ((length = in.read(buffer)) > 0){
+				out.write(buffer, 0, length);
+			}
+			in.close();
+			out.flush();
+		} 
+	}
+
+	/**
+	 *
+	 */
+	protected void doPost (HttpServletRequest request,
+		HttpServletResponse response) throws ServletException, IOException
+	{
+
+	}
+}
