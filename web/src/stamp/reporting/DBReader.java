5ef7cbfa73ccd96c019be4af359436482cb12a96
Saswat Anand <saswat@stamp.(none)>
2013-10-24 12:08:23 -0700

    summary stuff

--- web/src/stamp/reporting/DBReader.java
+++ web/src/stamp/reporting/DBReader.java
@@ -1,31 +1,67 @@
 package stamp.reporting;
 
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.ServletException;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.logging.Logger;
+
 import java.sql.*;
 
-public class DBReader
+@WebServlet(name="DBReader", urlPatterns={"/dbreader"})
+public class DBReader extends HttpServlet
 {
-	public static void main(String args[])
+	public DBReader() 
 	{
-		String dbPath = args[0];
+		super();
+	}
 
+	/**
+	 * GET requests expected include:
+	 *   > Request for list of known srcs or sinks
+	 *  
+	 */
+	protected void doGet (HttpServletRequest request,
+		HttpServletResponse response) 
+	throws ServletException, IOException
+	{
+		String dbPath = request.getParameter("dbpath");
+		System.out.println("dbPath: "+dbPath);
 		Connection c = null;
 		try {
 			Class.forName("org.sqlite.JDBC");
 			c = DriverManager.getConnection("jdbc:sqlite:"+dbPath);
 			System.out.println("Opened database successfully");
 			Statement stmt = c.createStatement();
-			ResultSet rs = stmt.executeQuery( "SELECT * FROM FLOWS;" );
-			while ( rs.next() ) {
-				int id = rs.getInt("flowKey");
+			ResultSet rs = stmt.executeQuery( "SELECT appName FROM flows;" );
+			StringBuilder builder = new StringBuilder("[");
+			boolean first = true;
+			while (rs.next()) {
+				//int id = rs.getInt("flowKey");
 				String appName = rs.getString("appName");
-				String sourceLabel = rs.getString("sourceLabel");
-				String sinkLabel = rs.getString("sinkLabel");
-				System.out.println( appName + " " + sourceLabel +" " +sinkLabel);
+				System.out.println(appName);
+				//String sourceLabel = rs.getString("sourceLabel");
+				//String sinkLabel = rs.getString("sinkLabel");
+				//System.out.println( appName + " " + sourceLabel +" " +sinkLabel);
+				//writer.println(appName + " " + sourceLabel +" " +sinkLabel);
+				if(first)
+					first = false;
+				else
+					builder.append(", ");
+				builder.append("{\"app\": \""+appName+"\", \"flows\": []"+"}");
 			}
-
+			builder.append("]");
+			System.out.println(builder.toString());
+			PrintWriter writer = response.getWriter();
+			writer.println(builder.toString());
+			writer.flush();
 		} catch ( Exception e ) {
 			System.err.println( e.getClass().getName() + ": " + e.getMessage() );
-			System.exit(0);
 		}
 	}
 }
