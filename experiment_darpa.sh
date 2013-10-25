0ee261d9177ed5a65ebf62fd0b7f1b9792e4ccb8
Osbert Bastani <obastani@stanford.edu>
2013-10-25 12:02:25 -0700

    added new experiment script

---
+++ experiment_darpa.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+
+echo ./stamp analyze ../stamptest/DarpaApps/1A_AndorsTrail
+./stamp analyze ../stamptest/DarpaApps/1A_AndorsTrail
+echo ./stamp analyze ../stamptest/DarpaApps/1A_AndroidGame
+./stamp analyze ../stamptest/DarpaApps/1A_AndroidGame
+echo ./stamp analyze ../stamptest/DarpaApps/1A_Butane
+./stamp analyze ../stamptest/DarpaApps/1A_Butane
+echo ./stamp analyze ../stamptest/DarpaApps/1A_SysMon
+./stamp analyze ../stamptest/DarpaApps/1A_SysMon
+echo ./stamp analyze ../stamptest/DarpaApps/1A_SysWatcherB
+./stamp analyze ../stamptest/DarpaApps/1A_SysWatcherB
+echo ./stamp analyze ../stamptest/DarpaApps/1B_SysWatcherA
+./stamp analyze ../stamptest/DarpaApps/1B_SysWatcherA
+echo ./stamp analyze ../stamptest/DarpaApps/1C_tomdroid
+./stamp analyze ../stamptest/DarpaApps/1C_tomdroid
+echo ./stamp analyze ../stamptest/DarpaApps/2A_AndroidIRC
+./stamp analyze ../stamptest/DarpaApps/2A_AndroidIRC
+echo ./stamp analyze ../stamptest/DarpaApps/2A_AppLocker
+./stamp analyze ../stamptest/DarpaApps/2A_AppLocker
+echo ./stamp analyze ../stamptest/DarpaApps/2A_AutoQuiet
+./stamp analyze ../stamptest/DarpaApps/2A_AutoQuiet
+echo ./stamp analyze ../stamptest/DarpaApps/2A_backupHelper
+./stamp analyze ../stamptest/DarpaApps/2A_backupHelper
+echo ./stamp analyze ../stamptest/DarpaApps/2A_bites
+./stamp analyze ../stamptest/DarpaApps/2A_bites
+echo ./stamp analyze ../stamptest/DarpaApps/2A_DeviceAdmin2
+./stamp analyze ../stamptest/DarpaApps/2A_DeviceAdmin2
+echo ./stamp analyze ../stamptest/DarpaApps/2A_Expenses
+./stamp analyze ../stamptest/DarpaApps/2A_Expenses
+echo ./stamp analyze ../stamptest/DarpaApps/2A_GeoTagger
+./stamp analyze ../stamptest/DarpaApps/2A_GeoTagger
+echo ./stamp analyze ../stamptest/DarpaApps/2A_InstantMessage
+./stamp analyze ../stamptest/DarpaApps/2A_InstantMessage
+echo ./stamp analyze ../stamptest/DarpaApps/2A_MyPolicy
+./stamp analyze ../stamptest/DarpaApps/2A_MyPolicy
+echo ./stamp analyze ../stamptest/DarpaApps/2A_podcast
+./stamp analyze ../stamptest/DarpaApps/2A_podcast
+echo ./stamp analyze ../stamptest/DarpaApps/2A_QuizMaster
+./stamp analyze ../stamptest/DarpaApps/2A_QuizMaster
+echo ./stamp analyze ../stamptest/DarpaApps/2A_SnapshotShare
+./stamp analyze ../stamptest/DarpaApps/2A_SnapshotShare
+echo ./stamp analyze ../stamptest/DarpaApps/2A_SortingApp
+./stamp analyze ../stamptest/DarpaApps/2A_SortingApp
+echo ./stamp analyze ../stamptest/DarpaApps/2A_UdonLauncher
+./stamp analyze ../stamptest/DarpaApps/2A_UdonLauncher
+echo ./stamp analyze ../stamptest/DarpaApps/2A_VideoGame
+./stamp analyze ../stamptest/DarpaApps/2A_VideoGame
+echo ./stamp analyze ../stamptest/DarpaApps/2B_AndroidMap
+./stamp analyze ../stamptest/DarpaApps/2B_AndroidMap
+echo ./stamp analyze ../stamptest/DarpaApps/2B_DvorakKeyboard
+./stamp analyze ../stamptest/DarpaApps/2B_DvorakKeyboard
+echo ./stamp analyze ../stamptest/DarpaApps/2B_fillinfun
+./stamp analyze ../stamptest/DarpaApps/2B_fillinfun
+echo ./stamp analyze ../stamptest/DarpaApps/2B_InstantReplay
+./stamp analyze ../stamptest/DarpaApps/2B_InstantReplay
+echo ./stamp analyze ../stamptest/DarpaApps/2B_NetPhone
+./stamp analyze ../stamptest/DarpaApps/2B_NetPhone
+echo ./stamp analyze ../stamptest/DarpaApps/2B_RunningApp
+./stamp analyze ../stamptest/DarpaApps/2B_RunningApp
+echo ./stamp analyze ../stamptest/DarpaApps/2B_SMSBot
+./stamp analyze ../stamptest/DarpaApps/2B_SMSBot
+echo ./stamp analyze ../stamptest/DarpaApps/2B_WhereMyAppsAt
+./stamp analyze ../stamptest/DarpaApps/2B_WhereMyAppsAt
