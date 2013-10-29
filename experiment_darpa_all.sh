530e745220a00bf8a60b6e3ecd7f90cec6d0a8cc
Osbert Bastani <obastani@stanford.edu>
2013-10-28 19:32:40 -0700

    added experiment for all darpa apps

---
+++ experiment_darpa_all.sh
@@ -0,0 +1,170 @@
+#!/bin/bash
+
+echo ./stamp analyze 0_GCache
+./stamp analyze 0_GCache
+echo ./stamp analyze 0_GpsCompass
+./stamp analyze 0_GpsCompass
+echo ./stamp analyze 0_notepad
+./stamp analyze 0_notepad
+echo ./stamp analyze 0_Sip2Peer
+./stamp analyze 0_Sip2Peer
+echo ./stamp analyze 0_TheBarcodeScanner
+./stamp analyze 0_TheBarcodeScanner
+echo ./stamp analyze 0_TheKeyboard
+./stamp analyze 0_TheKeyboard
+echo ./stamp analyze 0_TRAQ
+./stamp analyze 0_TRAQ
+echo ./stamp analyze 0_UltraCoolMap
+./stamp analyze 0_UltraCoolMap
+echo ./stamp analyze 1A_AndorsTrail
+./stamp analyze 1A_AndorsTrail
+echo ./stamp analyze 1A_AndroidGame
+./stamp analyze 1A_AndroidGame
+echo ./stamp analyze 1A_APG-E
+./stamp analyze 1A_APG-E
+echo ./stamp analyze 1A_Butane
+./stamp analyze 1A_Butane
+echo ./stamp analyze 1A_CalcA
+./stamp analyze 1A_CalcA
+echo ./stamp analyze 1A_CalcB
+./stamp analyze 1A_CalcB
+echo ./stamp analyze 1A_ConnectBot
+./stamp analyze 1A_ConnectBot
+echo ./stamp analyze 1A_CountdownTimer
+./stamp analyze 1A_CountdownTimer
+echo ./stamp analyze 1A_FunDraw
+./stamp analyze 1A_FunDraw
+echo ./stamp analyze 1A_MorseCode
+./stamp analyze 1A_MorseCode
+echo ./stamp analyze 1A_MyDrawA
+./stamp analyze 1A_MyDrawA
+echo ./stamp analyze 1A_MyDrawC
+./stamp analyze 1A_MyDrawC
+echo ./stamp analyze 1A_NewsCollator
+./stamp analyze 1A_NewsCollator
+echo ./stamp analyze 1A_PasswordSaver
+./stamp analyze 1A_PasswordSaver
+echo ./stamp analyze 1A_PersistentAssistant
+./stamp analyze 1A_PersistentAssistant
+echo ./stamp analyze 1A_SmartcamWebcam
+./stamp analyze 1A_SmartcamWebcam
+echo ./stamp analyze 1A_SMSReminder
+./stamp analyze 1A_SMSReminder
+echo ./stamp analyze 1A_SourceViewer
+./stamp analyze 1A_SourceViewer
+echo ./stamp analyze 1A_SplitTimer
+./stamp analyze 1A_SplitTimer
+echo ./stamp analyze 1A_SuperNote
+./stamp analyze 1A_SuperNote
+echo ./stamp analyze 1A_SuperSudoku
+./stamp analyze 1A_SuperSudoku
+echo ./stamp analyze 1A_SysMon
+./stamp analyze 1A_SysMon
+echo ./stamp analyze 1A_SysWatcherB
+./stamp analyze 1A_SysWatcherB
+echo ./stamp analyze 1A_TextSecure
+./stamp analyze 1A_TextSecure
+echo ./stamp analyze 1A_ToDoList
+./stamp analyze 1A_ToDoList
+echo ./stamp analyze 1A_WordHelper
+./stamp analyze 1A_WordHelper
+echo ./stamp analyze 1B_AndBible
+./stamp analyze 1B_AndBible
+echo ./stamp analyze 1B_APG-M
+./stamp analyze 1B_APG-M
+echo ./stamp analyze 1B_BatteryIndicator
+./stamp analyze 1B_BatteryIndicator
+echo ./stamp analyze 1B_CalcF
+./stamp analyze 1B_CalcF
+echo ./stamp analyze 1B_MediaFun
+./stamp analyze 1B_MediaFun
+echo ./stamp analyze 1B_MyDrawD
+./stamp analyze 1B_MyDrawD
+echo ./stamp analyze 1B_OpenGPSTracker
+./stamp analyze 1B_OpenGPSTracker
+echo ./stamp analyze 1B_Orienteering
+./stamp analyze 1B_Orienteering
+echo ./stamp analyze 1B_PicViewer
+./stamp analyze 1B_PicViewer
+echo ./stamp analyze 1B_Shareloc
+./stamp analyze 1B_Shareloc
+echo ./stamp analyze 1B_smsbackup
+./stamp analyze 1B_smsbackup
+echo ./stamp analyze 1B_SMSBlocker
+./stamp analyze 1B_SMSBlocker
+echo ./stamp analyze 1B_SmsPopup
+./stamp analyze 1B_SmsPopup
+echo ./stamp analyze 1B_SysWatcherA
+./stamp analyze 1B_SysWatcherA
+echo ./stamp analyze 1B_UltraCoolMap
+./stamp analyze 1B_UltraCoolMap
+echo ./stamp analyze 1B_YARR
+./stamp analyze 1B_YARR
+echo ./stamp analyze 1C_AndroidsFortune
+./stamp analyze 1C_AndroidsFortune
+echo ./stamp analyze 1C_CalcC
+./stamp analyze 1C_CalcC
+echo ./stamp analyze 1C_CalcE
+./stamp analyze 1C_CalcE
+echo ./stamp analyze 1C_ColorMatcher
+./stamp analyze 1C_ColorMatcher
+echo ./stamp analyze 1C_FullControl
+./stamp analyze 1C_FullControl
+echo ./stamp analyze 1C_KitteyKittey
+./stamp analyze 1C_KitteyKittey
+echo ./stamp analyze 1C_Orienteering
+./stamp analyze 1C_Orienteering
+echo ./stamp analyze 1C_Sanity
+./stamp analyze 1C_Sanity
+echo ./stamp analyze 1C_tomdroid
+./stamp analyze 1C_tomdroid
+echo ./stamp analyze 1C_WIFINDER
+./stamp analyze 1C_WIFINDER
+echo ./stamp analyze 2A_AndroidIRC
+./stamp analyze 2A_AndroidIRC
+echo ./stamp analyze 2A_AppLocker
+./stamp analyze 2A_AppLocker
+echo ./stamp analyze 2A_AutoQuiet
+./stamp analyze 2A_AutoQuiet
+echo ./stamp analyze 2A_backupHelper
+./stamp analyze 2A_backupHelper
+echo ./stamp analyze 2A_bites
+./stamp analyze 2A_bites
+echo ./stamp analyze 2A_DeviceAdmin2
+./stamp analyze 2A_DeviceAdmin2
+echo ./stamp analyze 2A_Expenses
+./stamp analyze 2A_Expenses
+echo ./stamp analyze 2A_GeoTagger
+./stamp analyze 2A_GeoTagger
+echo ./stamp analyze 2A_InstantMessage
+./stamp analyze 2A_InstantMessage
+echo ./stamp analyze 2A_MyPolicy
+./stamp analyze 2A_MyPolicy
+echo ./stamp analyze 2A_podcast
+./stamp analyze 2A_podcast
+echo ./stamp analyze 2A_QuizMaster
+./stamp analyze 2A_QuizMaster
+echo ./stamp analyze 2A_SnapshotShare
+./stamp analyze 2A_SnapshotShare
+echo ./stamp analyze 2A_SortingApp
+./stamp analyze 2A_SortingApp
+echo ./stamp analyze 2A_UdonLauncher
+./stamp analyze 2A_UdonLauncher
+echo ./stamp analyze 2A_VideoGame
+./stamp analyze 2A_VideoGame
+echo ./stamp analyze 2B_AndroidMap
+./stamp analyze 2B_AndroidMap
+echo ./stamp analyze 2B_DvorakKeyboard
+./stamp analyze 2B_DvorakKeyboard
+echo ./stamp analyze 2B_fillinfun
+./stamp analyze 2B_fillinfun
+echo ./stamp analyze 2B_InstantReplay
+./stamp analyze 2B_InstantReplay
+echo ./stamp analyze 2B_NetPhone
+./stamp analyze 2B_NetPhone
+echo ./stamp analyze 2B_RunningApp
+./stamp analyze 2B_RunningApp
+echo ./stamp analyze 2B_SMSBot
+./stamp analyze 2B_SMSBot
+echo ./stamp analyze 2B_WhereMyAppsAt
+./stamp analyze 2B_WhereMyAppsAt
