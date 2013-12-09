bbd210d156b4be08ebdec7b09975d8da57d8565c
Saswat Anand <saswat@stamplabs.(none)>
2013-12-09 02:14:25 -0800

    ant -Dapk.in=foo.apk -Dapk.out=final.apk

---
+++ obfuscate/src/com/apposcopy/obfuscate/FixManifest.java
@@ -0,0 +1,170 @@
+package com.apposcopy.obfuscate;
+
+import java.util.*;
+import java.io.*;
+import javax.xml.transform.*;
+import javax.xml.*;
+import javax.xml.parsers.*;
+import javax.xml.xpath.*;
+import javax.xml.namespace.*;
+import org.w3c.dom.*;
+import org.xml.sax.*;
+import javax.xml.transform.dom.*;
+import javax.xml.transform.stream.StreamResult;
+
+public class FixManifest
+{
+	Document document;
+	String pkgName; 
+	XPath xpath;
+	Mapper mapper;
+
+	FixManifest(String mappingFile)
+	{
+		this.mapper = new Mapper(mappingFile);
+	}
+
+	void fixManifest(String manifestFile) 
+	{
+		try{
+			DocumentBuilder builder =
+				DocumentBuilderFactory.newInstance().newDocumentBuilder();
+			this.document = builder.parse(manifestFile);
+			
+			this.xpath = XPathFactory.newInstance().newXPath();
+			xpath.setNamespaceContext(new PersonalNamespaceContext());
+		
+			//find package name
+			Node node = (Node)
+				xpath.evaluate("/manifest", document, XPathConstants.NODE);
+			this.pkgName = node.getAttributes().getNamedItem("package").getNodeValue();
+			
+			for(String compType : new String[]{"activity", "service", "receiver"}){
+				fixCompNames(compType);
+			}
+			
+			writeToFile(manifestFile);
+		}catch(Exception e){
+			throw new Error(e);
+		}
+	}
+	
+	private String fixName(String comp)
+	{
+		if(comp.startsWith("."))
+			comp = pkgName + comp;
+		else if(comp.indexOf('.') < 0)
+			comp = pkgName + "." + comp;
+		comp = mapper.oldToNewClassName.get(comp);
+		return comp;
+	}
+
+	private void fixCompNames(String componentType) throws Exception
+	{
+		NodeList nodes = (NodeList)
+			xpath.evaluate("/manifest/application/"+componentType, document, XPathConstants.NODESET);
+		for (int i = 0; i < nodes.getLength(); i++) {
+			Node node = nodes.item(i);
+			NamedNodeMap nnm = node.getAttributes();
+			String name = null;
+			for(int j = 0; j < nnm.getLength(); j++){
+				Node n = nnm.item(j);
+				if(n.getNodeName().equals("android:name")){
+					name = n.getNodeValue();
+					n.setNodeValue(fixName(name));
+					break;
+				}
+				//System.out.println(n.getNodeName() + " " + );
+			}			
+			assert name != null : node.getNodeName();
+		}
+	}
+
+	void writeToFile(String fileName)
+	{
+		Source source = new DOMSource(document);
+		Result result = new StreamResult(new File(fileName));
+		Transformer xformer;
+		try {
+			xformer = TransformerFactory.newInstance().newTransformer();
+			xformer.transform(source, result);
+		} catch (TransformerConfigurationException e) {
+			throw new Error(e);
+		} catch (TransformerFactoryConfigurationError e) {
+			throw new Error(e);
+		} catch (TransformerException e) {
+			throw new Error(e);
+		}
+	}
+
+	public static void main(String[] args)
+	{
+		String mappingFile = args[0];
+		String manifestFile = args[1];
+		
+		FixManifest fm = new FixManifest(mappingFile);
+		fm.fixManifest(manifestFile);
+	}
+
+	/*
+	private static void fixManifest(Mapper mapper, String manifestFile) throws Exception
+	{
+		File tmpFile = File.createTempFile("stamp_android_manifest", null, null);
+		tmpFile.deleteOnExit();
+		PrintWriter writer = new PrintWriter(new BufferedWriter(new FileWriter(tmpFile)));
+
+		BufferedReader reader = new BufferedReader(new FileReader(manifestFile));
+		String line;
+		while((line = reader.readLine()) != null){
+			line = process(line);
+			writer.println(line);
+		}
+		reader.close();
+		writer.close();
+		
+		copy(tmpFile, new File(manifestFile));
+	}
+
+
+	private static final int DEFAULT_BUFFER_SIZE = 10240; // 10KB.
+
+	public static void copy(File srcFile, File dstFile) throws IOException
+	{
+		InputStream input = null;
+		OutputStream output = null;
+		try {
+			input = new BufferedInputStream(new FileInputStream(srcFile), DEFAULT_BUFFER_SIZE);
+			output = new BufferedOutputStream(new FileOutputStream(dstFile), DEFAULT_BUFFER_SIZE);
+			byte[] buffer = new byte[DEFAULT_BUFFER_SIZE];
+			for (int length = 0; ((length = input.read(buffer)) > 0);) {
+				output.write(buffer, 0, length);
+			}
+		} finally {
+			if (output != null) try { output.close(); } catch (IOException e) { throw e; }
+			if (input != null) try { input.close(); } catch (IOException e) { throw e; }
+		}
+	}
+	*/
+
+}
+
+class PersonalNamespaceContext implements NamespaceContext 
+{
+	public String getNamespaceURI(String prefix) {
+		if (prefix == null) throw new NullPointerException("Null prefix");
+		else if ("android".equals(prefix)) return "http://schemas.android.com/apk/res/android";
+		else if ("xml".equals(prefix)) return XMLConstants.XML_NS_URI;
+			return XMLConstants.NULL_NS_URI;
+	}
+	
+	// This method isn't necessary for XPath processing.
+	public String getPrefix(String uri) {
+		throw new UnsupportedOperationException();
+	}
+	
+	// This method isn't necessary for XPath processing either.
+	public Iterator getPrefixes(String uri) {
+		throw new UnsupportedOperationException();
+	}
+	
+}
---
+++ obfuscate/src/com/apposcopy/obfuscate/Mapper.java
@@ -0,0 +1,43 @@
+package com.apposcopy.obfuscate;
+
+import java.util.*;
+import java.io.*;
+
+public class Mapper
+{
+	public Map<String,String> oldToNewClassName = new HashMap();
+	private String mappingFile;
+
+	Mapper(String mappingFile)
+	{
+		this.mappingFile = mappingFile;
+		readMappingFile();
+	}
+
+	public void readMappingFile()
+	{
+		try{
+			BufferedReader reader = new BufferedReader(new FileReader(mappingFile));
+			String line;
+			while((line = reader.readLine()) != null){
+				if(line.endsWith(":")){
+					line = line.substring(0, line.length()-1);
+					int i = line.indexOf("->");
+					String oldName = line.substring(0, i-1);
+					String newName = line.substring(i+3);
+					System.out.println(oldName+" "+newName);
+					oldToNewClassName.put(oldName, newName);
+			}
+			}
+			reader.close();
+		}catch(IOException e){
+			throw new Error(e);
+		}
+	}
+	
+	public String newName(String s)
+	{
+		return oldToNewClassName.get(s);
+	}
+	
+}
