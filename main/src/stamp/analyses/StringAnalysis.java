bebabef11fb12229ea7559a3d228d8bef8327653
Saswat Anand <saswat@stamplabs.(none)>
2014-01-16 09:26:54 -0800

    adding header strings

--- main/src/stamp/analyses/StringAnalysis.java
+++ main/src/stamp/analyses/StringAnalysis.java
@@ -5,8 +5,13 @@ import soot.SootMethod;
 import soot.Unit;
 import soot.Value;
 import soot.ValueBox;
+import soot.Scene;
 import soot.jimple.Stmt;
 import soot.jimple.StringConstant;
+import soot.jimple.toolkits.callgraph.CallGraph;
+import soot.jimple.toolkits.callgraph.Edge;
+
+import chord.util.tuple.object.Pair;
 
 import shord.program.Program;
 import stamp.srcmap.sourceinfo.abstractinfo.AbstractSourceInfo;
@@ -36,6 +41,39 @@ public class StringAnalysis
 				scs.add(str);
 			}
 		}
+		
+		Scene scene = Program.g().scene();
+		CallGraph cg = scene.getCallGraph();
 
+		List<Pair<String,Integer>> httpHeaderMeths = new ArrayList();
+		httpHeaderMeths.add(new Pair("<org.apache.http.client.methods.HttpGet: void addHeader(java.lang.String,java.lang.String)>", 1));
+		httpHeaderMeths.add(new Pair("<org.apache.http.client.methods.HttpGet: void addHeader(java.lang.String,java.lang.String)>", 2));
+		httpHeaderMeths.add(new Pair("<org.apache.http.client.methods.HttpPost: void addHeader(java.lang.String,java.lang.String)>", 1));
+		httpHeaderMeths.add(new Pair("<org.apache.http.client.methods.HttpPost: void addHeader(java.lang.String,java.lang.String)>", 2));
+		httpHeaderMeths.add(new Pair("<org.apache.http.message.BasicHeader: void <init>(java.lang.String,java.lang.String)>", 1));
+		httpHeaderMeths.add(new Pair("<org.apache.http.message.BasicHeader: void <init>(java.lang.String,java.lang.String)>", 2));
+		
+		httpHeaderMeths.add(new Pair("<org.apache.http.params.DefaultedHttpParams: org.apache.http.params.HttpParams setParameter(java.lang.String,java.lang.Object)>", 1));
+		httpHeaderMeths.add(new Pair("<org.apache.http.params.BasicHttpParams: org.apache.http.params.HttpParams setParameter(java.lang.String,java.lang.Object)>", 1));
+		httpHeaderMeths.add(new Pair("<org.apache.http.params.AbstractHttpParams: org.apache.http.params.HttpParams setBooleanParameter(java.lang.String,boolean)>", 1));
+		httpHeaderMeths.add(new Pair("<org.apache.http.params.AbstractHttpParams: org.apache.http.params.HttpParams setDoubleParameter(java.lang.String,double)>", 1));
+		httpHeaderMeths.add(new Pair("<org.apache.http.params.AbstractHttpParams: org.apache.http.params.HttpParams setIntParameter(java.lang.String,int)>", 1));
+		httpHeaderMeths.add(new Pair("<org.apache.http.params.AbstractHttpParams: org.apache.http.params.HttpParams setLongParameter(java.lang.String,long)>", 1));
+
+		for(Pair<String,Integer> pair : httpHeaderMeths){
+			String mSig = pair.val0;
+			if(!scene.containsMethod(mSig))
+				continue;
+			SootMethod m = scene.getMethod(mSig);
+			Integer paramIndex = pair.val1;
+
+			Iterator<Edge> edgeIt = cg.edgesInto(m);
+			while(edgeIt.hasNext()){
+				Edge edge = edgeIt.next();
+				Stmt stmt = edge.srcStmt();
+				SootMethod src = edge.src();
+				System.out.println("headerstmt: "+stmt + " in " + src.getSignature());
+			}
+		}
 	}
 }
