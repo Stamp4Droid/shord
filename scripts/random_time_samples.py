f3e37aa10f3be95765a1670372b937602dd733dc
brycecr <brycecr@gmail.com>
2014-02-10 15:01:45 -0800

    fixing up random sample script to be a bit more user friendly. also requires argparse now.

--- scripts/random_time_samples.py
+++ scripts/random_time_samples.py
@@ -5,28 +5,38 @@ import subprocess
 import random
 import time
 import csv
+import argparse
+
+def parseargs():
+    parser = argparse.ArgumentParser(description="Automatically run apps and determine their success/failure and time to complete")
+    parser.add_argument("appdir", type=str, help="the directory of apps to select from. all apps should be at the top level and the directory should only contain apps")
+    parser.add_argument("-o", "--output", type=str, help="output file; will be csv and will be opened in append mode", default="random_samples.csv")
+    parser.add_argument("-s", "--stamp_loc", type=str, help="path to stamp script from here", default="../stamp")
+    parser.add_argument("-i", "--iterations", type=int, help="number of iterations to run", default=1)
+    args = parser.parse_args()
+    return (args.appdir, args.output, args.stamp_loc, args.iterations)
+    
 
 if __name__ == "__main__":
-	if len(sys.argv) < 4:
-		exit("Need 4 arguments; apps dir; stamp binary; number of samples")
+        (appdir, output, stamp_loc, iterations) = parseargs()
 
-	apps = os.listdir(sys.argv[1])
+	apps = os.listdir(appdir)
 	num_apps = len(apps)
 	print("There are "+str(num_apps)+" apps in the directory...randomly sampling...")
 
 	already_sampled = []
 	timings = {}
-	w = csv.writer(open("random_samples.csv", "a"))
+	w = csv.writer(open(output, "a"))
 
-	for i in range(0,int(sys.argv[3])):
+	for i in range(0,int(iterations)):
 		j = random.randint(1, num_apps)
 		while already_sampled.count(j) > 0:
 			j = random.randint(1, num_apps)
 			
 		command = []
-		command.append(sys.argv[2])
+		command.append(stamp_loc)
 		command.append('analyze')
-		command.append(sys.argv[1]+apps[j])
+		command.append(appdir+apps[j])
 		print(' '.join(command))
 		start_time = time.time()
 		exit_code = subprocess.call(command)
