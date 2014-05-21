1cbafe72208901c36bd7a757782e005ab1ad7597
Manolis Papadakis <manopapad@gmail.com>
2014-05-20 17:24:31 -0700

    Combine all visualization transformations into one script

--- solvergen/combine.py
+++ solvergen/combine.py
@@ -1,7 +1,13 @@
 #!/usr/bin/env python
 
 import argparse
+import os
 import re
+import subprocess
+import tempfile
+import tgf2dot
+
+# =============================================================================
 
 head = """<html>
 <head>
@@ -25,53 +31,82 @@ g.edge>a:hover>path {
 tail = """</body>
 </html>"""
 
-# =============================================================================
-
-def print_svg(fname, id_prefix):
+def print_svg(svg_file, id_prefix):
     skip = 6
     header = 3
-    with open(fname) as f:
-        for line in f:
-            if skip > 0:
-                skip -= 1
-                continue
-            if header > 0:
-                header -= 1
-                if header == 0:
-                    print style_decl
-            if id_prefix is not None:
-                line = re.sub(r'id="([^\"]+)"',
-                              r'id="%s::\1"' % id_prefix, line)
-            print line[:-1]
+    for line in svg_file:
+        if skip > 0:
+            skip -= 1
+            continue
+        if header > 0:
+            header -= 1
+            if header == 0:
+                print style_decl
+        if id_prefix is not None:
+            line = re.sub(r'id="([^\"]+)"',
+                          r'id="%s::\1"' % id_prefix, line)
+        print line[:-1]
+    svg_file.seek(0)
+
+def print_html(graph_svg, pri_svg, sec_svgs, effects_fname, vmap_fname):
+    print head
+    print_svg(graph_svg, '0')
+    print "</br>"
+    print_svg(pri_svg, '1')
+    print "</br>"
+    for c in sec_svgs:
+        print_svg(c, '2')
+    print "</br>"
+    for c in sec_svgs:
+        print_svg(c, '3')
+    print '<script>'
+    with open(effects_fname) as fin:
+        for line in fin:
+            toks = line.split()
+            print ('recordEffect("%s","%s","%s","%s","%s");' %
+                   (toks[0], toks[1], toks[2], toks[3], ' '.join(toks[4:])))
+    i = 0
+    if os.path.exists(vmap_fname):
+        with open(vmap_fname) as fin:
+            for line in fin:
+                print 'recordName("%s","%s");' % ('v%s' % i, line[:-1])
+                i += 1
+    print '</script>'
+    print tail
+
+# =============================================================================
+
+def list_tgfs(rsm_dname):
+    return [os.path.join(rsm_dname, tgf_fbase)
+            for tgf_fbase in os.listdir(rsm_dname)
+            if tgf_fbase.endswith('.tgf')]
+
+def dot2svg(dot_file):
+    svg_file = tempfile.TemporaryFile(suffix='svg')
+    subprocess.check_call(['dot', '-Tsvg'], stdin=dot_file, stdout=svg_file)
+    svg_file.seek(0)
+    return svg_file
+
+def tgf2svg(tgf_fname):
+    with tempfile.TemporaryFile(suffix='dot') as dot_file:
+        tgf2dot.convert(tgf_fname, dot_file)
+        dot_file.seek(0)
+        return dot2svg(dot_file)
 
 # =============================================================================
 
 parser = argparse.ArgumentParser()
-parser.add_argument('v_map', type=argparse.FileType('r'))
-parser.add_argument('effects', type=argparse.FileType('r'))
-parser.add_argument('graph')
 parser.add_argument('pri_comp')
-parser.add_argument('sec_comps', nargs="+")
+parser.add_argument('sec_dir')
+parser.add_argument('app_out_dir')
+parser.add_argument('start')
 args = parser.parse_args()
 
-print head
-print_svg(args.graph, '0')
-print "</br>"
-print_svg(args.pri_comp, '1')
-print "</br>"
-for c in args.sec_comps:
-    print_svg(c, '2')
-print "</br>"
-for c in args.sec_comps:
-    print_svg(c, '3')
-print '<script>'
-for line in args.effects:
-    toks = line.split()
-    print ('recordEffect("%s","%s","%s","%s","%s");' %
-           (toks[0], toks[1], toks[2], toks[3], ' '.join(toks[4:])))
-i = 0
-for line in args.v_map:
-    print 'recordName("%s","%s");' % ('v%s' % i, line[:-1])
-    i += 1
-print '</script>'
-print tail
+with open(os.path.join(args.app_out_dir, 'dump',
+                       args.start + '.dot')) as graph_dot:
+    graph_svg = dot2svg(graph_dot)
+pri_svg = tgf2svg(args.pri_comp)
+sec_svgs = [tgf2svg(c) for c in list_tgfs(args.sec_dir)]
+effects_fname = os.path.join(args.app_out_dir, 'dump', args.start + '.summs')
+vmap_fname = os.path.join(args.app_out_dir, 'chord_output', 'bddbddb', 'V.map')
+print_html(graph_svg, pri_svg, sec_svgs, effects_fname, vmap_fname)
--- solvergen/tgf2dot.py
+++ solvergen/tgf2dot.py
@@ -2,50 +2,54 @@
 
 import argparse
 from os.path import basename, splitext
+import sys
 
-parser = argparse.ArgumentParser()
-parser.add_argument('tgf_file')
-args = parser.parse_args()
+def convert(tgf_fname, fout):
+    comp = splitext(splitext(basename(tgf_fname))[0])[0]
+    states_done = False
+    trans = {}
 
-comp = splitext(splitext(basename(args.tgf_file))[0])[0]
-states_done = False
-trans = {}
+    fout.write('digraph %s {\n' % comp)
+    fout.write('id="%s";\n' % comp)
+    fout.write('rankdir="LR";\n')
+    fout.write('node [style="filled",fillcolor="white"];\n')
 
-print 'digraph %s {' % comp
-print 'id="%s";' % comp
-print 'rankdir="LR";'
-print 'node [style="filled",fillcolor="white"];'
+    with open(tgf_fname) as fin:
+        for line in fin:
+            toks = line.split()
+            if toks == []:
+                pass
+            elif states_done:
+                src = toks[0]
+                dst = toks[1]
+                trans[(src,dst)] = trans.get((src,dst), []) + toks[2:]
+            elif toks == ['#']:
+                states_done = True
+            else:
+                state = toks[0]
+                weight = ''
+                shape = 'circle'
+                label = ' '
+                for t in toks[1:]:
+                    if t == 'in':
+                        shape = 'octagon'
+                    elif t == 'out':
+                        weight = 'double'
+                    else:
+                        shape = 'box'
+                        label = t
+                fout.write('%s [id="%s",tooltip="%s",shape="%s",label="%s"];\n'
+                           % (state, comp + ':' + state, comp + ':' + state,
+                              weight + shape, label))
 
-with open(args.tgf_file) as fin:
-    for line in fin:
-        toks = line.split()
-        if toks == []:
-            pass
-        elif states_done:
-            src = toks[0]
-            dst = toks[1]
-            trans[(src,dst)] = trans.get((src,dst), []) + toks[2:]
-        elif toks == ['#']:
-            states_done = True
-        else:
-            state = toks[0]
-            weight = ''
-            shape = 'circle'
-            label = ' '
-            for t in toks[1:]:
-                if t == 'in':
-                    shape = 'octagon'
-                elif t == 'out':
-                    weight = 'double'
-                else:
-                    shape = 'box'
-                    label = t
-            print ('%s [id="%s",tooltip="%s",shape="%s",label="%s"];' %
-                   (state, comp + ':' + state, comp + ':' + state,
-                    weight + shape, label))
+    for (src,dst) in trans:
+        edge_id = comp + ':' + src + '->' + comp + ':' + dst
+        fout.write('%s -> %s [id="%s",tooltip="%s"];\n' %
+                   (src, dst, edge_id, ' '.join(trans[(src,dst)])))
+    fout.write('}\n')
 
-for (src,dst) in trans:
-    edge_id = comp + ':' + src + '->' + comp + ':' + dst
-    print ('%s -> %s [id="%s",tooltip="%s"];' %
-           (src, dst, edge_id, ' '.join(trans[(src,dst)])))
-print '}'
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('tgf_file')
+    args = parser.parse_args()
+    convert(args.tgf_file, sys.stdout)
