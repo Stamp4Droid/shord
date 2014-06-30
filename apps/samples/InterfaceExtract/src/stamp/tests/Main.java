13cf5a03f92c7ec68426f48e119a44b6f4002146
Saswat Anand <saswat@stamp.(none)>
2014-06-30 00:19:25 -0700

    example for textbox analysis

--- apps/samples/InterfaceExtract/src/stamp/tests/Main.java
+++ apps/samples/InterfaceExtract/src/stamp/tests/Main.java
@@ -2,6 +2,8 @@ package stamp.tests;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.content.Intent;
+import android.widget.EditText;
 
 public class Main extends Activity
 {
@@ -11,7 +13,13 @@ public class Main extends Activity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-		
-		int x = getIntent().getIntExtra("key", 10);
+
+		final EditText textBox1 = (EditText) this.findViewById(R.id.textbox1);
+		final EditText textBox2 = (EditText) this.findViewById(R.id.textbox2);
+
+		Intent i = new Intent(this, Second.class);
+		i.putExtra("key1", textBox1.getText().toString());
+		i.putExtra("key2", textBox2.getText().toString());
+		startActivity(i);
     }
 }
