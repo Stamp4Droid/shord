b16cb3559b9de0cf3613f4d00bba199df5954124
Osbert Bastani <obastani@stanford.edu>
2014-10-31 16:38:39 -0700

    updated tests callback to use callgraph

--- main/src/stamp/analyses/TestsCallbackAnalysis.java
+++ main/src/stamp/analyses/TestsCallbackAnalysis.java
@@ -9,12 +9,14 @@ import stamp.missingmodels.util.Util.Pair;
 import stamp.missingmodels.util.abduction.AbductiveInferenceRunner;
 import stamp.missingmodels.util.abduction.AbductiveInferenceRunner.AbductiveInferenceHelper;
 import stamp.missingmodels.util.abduction.AbductiveInferenceRunner.DefaultAbductiveInferenceHelper;
+import stamp.missingmodels.util.cflsolver.grammars.CallgraphTaintGrammar;
 import stamp.missingmodels.util.cflsolver.grammars.TaintGrammar.TaintPointsToGrammar;
 import stamp.missingmodels.util.cflsolver.graph.ContextFreeGrammar;
 import stamp.missingmodels.util.cflsolver.graph.Graph;
 import stamp.missingmodels.util.cflsolver.graph.Graph.Edge;
 import stamp.missingmodels.util.cflsolver.graph.Graph.EdgeFilter;
 import stamp.missingmodels.util.cflsolver.relation.CallbackRelationManager;
+import stamp.missingmodels.util.cflsolver.relation.DynamicCallgraphRelationManager;
 import stamp.missingmodels.util.cflsolver.relation.RelationReader;
 import stamp.missingmodels.util.cflsolver.relation.RelationReader.ShordRelationReader;
 import stamp.missingmodels.util.cflsolver.solver.ReachabilitySolver;
@@ -31,7 +33,8 @@ public class TestsCallbackAnalysis extends JavaAnalysis {
 				return gbar.getEdges(new EdgeFilter() {
 					@Override
 					public boolean filter(Edge edge) {
-						return (edge.getSymbol().equals("param") || edge.getSymbol().equals("paramPrim"))
+						//return (edge.getSymbol().equals("param") || edge.getSymbol().equals("paramPrim"))
+						return (edge.getSymbol().equals("callgraph"))
 								&& !baseEdgeFilter.get(edge.source.toString(true)).contains(edge.sink.toString(true));
 					}
 				});
@@ -80,17 +83,20 @@ public class TestsCallbackAnalysis extends JavaAnalysis {
 		try {
 			MultivalueMap<String,String> paramEdges = getGraphEdgesFromFile("param", "graph");
 			MultivalueMap<String,String> paramPrimEdges = getGraphEdgesFromFile("paramPrim", "graph");
+			MultivalueMap<String,String> callgraphEdges = getGraphEdgesFromFile("callgraph", "graph");
 			MultivalueMap<String,String> sourceSinkEdges = getGraphEdgesFromFile("Src2Sink", "graph");
-			AbductiveInferenceHelper h = IOUtils.relationFileExists("param", "graph") ? getAbductiveInferenceHelper(getUnion(paramEdges, paramPrimEdges), sourceSinkEdges) : new DefaultAbductiveInferenceHelper();
+			//AbductiveInferenceHelper h = IOUtils.relationFileExists("param", "graph") ? getAbductiveInferenceHelper(getUnion(paramEdges, paramPrimEdges), sourceSinkEdges) : new DefaultAbductiveInferenceHelper();
+			AbductiveInferenceHelper h = IOUtils.relationFileExists("param", "graph") ? getAbductiveInferenceHelper(callgraphEdges, sourceSinkEdges) : new DefaultAbductiveInferenceHelper();
 			
-			IOUtils.printRelation("reachableM");
-			IOUtils.printRelation("callgraph");
-			IOUtils.printRelation("potentialCallbackDependent");
+			//IOUtils.printRelation("reachableM");
+			//IOUtils.printRelation("callgraph");
+			//IOUtils.printRelation("potentialCallbackDependent");
 			
-			ContextFreeGrammar taintGrammar = new TaintPointsToGrammar();
+			//ContextFreeGrammar taintGrammar = new TaintPointsToGrammar();
+			ContextFreeGrammar taintGrammar = new CallgraphTaintGrammar();
 			RelationReader relationReader = new ShordRelationReader();
 			//Graph g = relationReader.readGraph(new CallbackRelationManager(paramEdges, paramPrimEdges), taintGrammar);
-			Graph g = relationReader.readGraph(new CallbackRelationManager(paramEdges, paramPrimEdges), taintGrammar);
+			Graph g = relationReader.readGraph(new DynamicCallgraphRelationManager(callgraphEdges), taintGrammar);
 			TypeFilter t = relationReader.readTypeFilter(taintGrammar);
 			IOUtils.printCallgraphAbductionResult(AbductiveInferenceRunner.runInference(h, g, t, true, 2), true);
 			
@@ -98,8 +104,8 @@ public class TestsCallbackAnalysis extends JavaAnalysis {
 			String extension = IOUtils.graphEdgesFileExists("param", "graph") ? "graph_new" : "graph";
 			IOUtils.printGraphEdgesToFile(gbar, "param", true, extension);
 			IOUtils.printGraphEdgesToFile(gbar, "paramPrim", true, extension);
+			IOUtils.printGraphEdgesToFile(gbar, "callgraph", true, extension);
 			IOUtils.printGraphEdgesToFile(gbar, "Src2Sink", true, extension);
-			
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
