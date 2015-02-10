fb6c8a858a31231a83680b3dc2187948c053fac3
Manolis Papadakis <manopapad@gmail.com>
2015-02-09 17:21:55 -0800

    Set memory limits before running the analysis

--- solvergen/rsm.sh
+++ solvergen/rsm.sh
@@ -3,4 +3,5 @@
 SGEN_DIR="$(cd "$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")" && pwd)"
 
 make -C "$SGEN_DIR" bin/rsm.out
+ulimit -v 10000000
 "$SGEN_DIR/bin/rsm.out" "$@"
--- solvergen/twodim.sh
+++ solvergen/twodim.sh
@@ -3,4 +3,5 @@
 SGEN_DIR="$(cd "$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")" && pwd)"
 
 make -C "$SGEN_DIR" bin/twodim.out
+ulimit -v 10000000
 export LD_LIBRARY_PATH="$SGEN_DIR/amore/"; "$SGEN_DIR/bin/twodim.out" "$@"
