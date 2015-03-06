e08e25b351afb87dadfc0c52711007f0b3e3c5a5
saswat Anand <saswat@kuhu2.stanford.edu>
2015-03-06 10:34:36 -0800

    fixing model

--- models/src/android/content/ContextWrapper.java
+++ models/src/android/content/ContextWrapper.java
@@ -30,7 +30,7 @@ class ContextWrapper
 		return new Intent();
 	}
 
-	@STAMP(flows = {@Flow(from="!Service.bind",to="service")})
+	@STAMP(flows = {@Flow(from="service",to="!Service")})
     public boolean bindService(android.content.Intent service, android.content.ServiceConnection conn, int flags) 
 	{
 		final android.content.ServiceConnection c = conn;
@@ -44,47 +44,45 @@ class ContextWrapper
 		return this;
 	}
 
-	@STAMP(flows = {@Flow(from="!Activity",to="intent"),@Flow(from="!Activity",to="options")})
+	@STAMP(flows = {@Flow(from="intent",to="!Activity"),@Flow(from="options",to="!Activity")})
 	public void startActivity(android.content.Intent intent, android.os.Bundle options) {
     }
 
-	@STAMP(flows = {@Flow(from="!Activity",to="intent")})
+	@STAMP(flows = {@Flow(from="intent",to="!Activity")})
 	public void startActivity(android.content.Intent intent) {
     }
 
-	@STAMP(flows = {@Flow(from="!Activity",to="intents"),@Flow(from="!Activity",to="options")})
+	@STAMP(flows = {@Flow(from="intents",to="!Activity"),@Flow(from="options",to="!Activity")})
 	public void startActivities(android.content.Intent[] intents, android.os.Bundle options) {
 	}
 
-	@STAMP(flows = {@Flow(from="!Activity",to="intents")})
+	@STAMP(flows = {@Flow(from="intents",to="!Activity")})
     public void startActivities(android.content.Intent[] intents) {
     }
 
-	@STAMP(flows = {@Flow(from="!Service.start",to="service")})
+	@STAMP(flows = {@Flow(from="service",to="!Service")})
     public android.content.ComponentName startService(android.content.Intent service) {
 		return null;
     }
 
-	@STAMP(flows = {@Flow(from="!Broadcast",to="intent")})
+	@STAMP(flows = {@Flow(from="intent",to="!Broadcast")})
 	public void sendBroadcast(android.content.Intent intent) {
     }
 
-	@STAMP(flows = {@Flow(from="!Broadcast",to="intent")})
+	@STAMP(flows = {@Flow(from="intent",to="!Broadcast")})
     public void sendBroadcast(android.content.Intent intent, java.lang.String receiverPermission) {
-		throw new RuntimeException("Stub!");
     }
 
-	@STAMP(flows = {@Flow(from="!Broadcast",to="intent")})
+	@STAMP(flows = {@Flow(from="intent",to="!Broadcast")})
     public void sendOrderedBroadcast(android.content.Intent intent, java.lang.String receiverPermission) {
-		throw new RuntimeException("Stub!");
     }
 
-	@STAMP(flows = {@Flow(from="!Broadcast",to="intent"),@Flow(from="!Broadcast",to="initialExtras")})
+	@STAMP(flows = {@Flow(from="intent",to="!Broadcast"),@Flow(from="initialExtras",to="!Broadcast")})
     public void sendOrderedBroadcast(android.content.Intent intent, java.lang.String receiverPermission, android.content.BroadcastReceiver resultReceiver, android.os.Handler scheduler, int initialCode, java.lang.String initialData, android.os.Bundle initialExtras) {
-        throw new RuntimeException("Stub!");
+
     }
 
-	@STAMP(flows = {@Flow(from="!Broadcast",to="intent")})
+	@STAMP(flows = {@Flow(from="intent",to="!Broadcast")})
     public void sendStickyBroadcast(android.content.Intent intent) {
     }
 
