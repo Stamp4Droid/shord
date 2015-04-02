4190c6c4c924a13754b15741166c31fa26542ff3
Osbert Bastani <obastani@gmail.com>
2015-04-02 14:57:51 -0700

    fixed mistake in cflsolveranalysis

--- main/src/stamp/analyses/CFLSolverAnalysis.java
+++ main/src/stamp/analyses/CFLSolverAnalysis.java
@@ -2,12 +2,10 @@ package stamp.analyses;
 
 import shord.project.analyses.JavaAnalysis;
 import stamp.missingmodels.util.cflsolver.core.ContextFreeGrammar.ContextFreeGrammarOpt;
-import stamp.missingmodels.util.cflsolver.core.Edge;
 import stamp.missingmodels.util.cflsolver.core.Graph;
 import stamp.missingmodels.util.cflsolver.core.ReachabilitySolver;
 import stamp.missingmodels.util.cflsolver.core.RelationManager;
 import stamp.missingmodels.util.cflsolver.core.RelationManager.RelationReader;
-import stamp.missingmodels.util.cflsolver.core.Util.Filter;
 import stamp.missingmodels.util.cflsolver.grammars.AliasModelsGrammar;
 import stamp.missingmodels.util.cflsolver.grammars.ImplicitFlowGrammar;
 import stamp.missingmodels.util.cflsolver.grammars.MissingRefRefGrammar.MissingRefRefImplicitFlowGrammar;
@@ -16,8 +14,8 @@ import stamp.missingmodels.util.cflsolver.grammars.TaintGrammar;
 import stamp.missingmodels.util.cflsolver.reader.ShordRelationReader;
 import stamp.missingmodels.util.cflsolver.relation.AliasModelsRelationManager;
 import stamp.missingmodels.util.cflsolver.relation.ImplicitFlowRelationManager;
-import stamp.missingmodels.util.cflsolver.relation.MissingRefRefRelationManager;
 import stamp.missingmodels.util.cflsolver.relation.MissingRefRefRelationManager.MissingRefRefImplicitFlowRelationManager;
+import stamp.missingmodels.util.cflsolver.relation.MissingRefRefRelationManager.MissingRefRefTaintRelationManager;
 import stamp.missingmodels.util.cflsolver.relation.SourceSinkRelationManager;
 import stamp.missingmodels.util.cflsolver.relation.TaintWithContextRelationManager;
 import stamp.missingmodels.util.cflsolver.util.IOUtils;
@@ -41,7 +39,7 @@ public class CFLSolverAnalysis extends JavaAnalysis {
 	public void run() {
 		run(new ShordRelationReader(), new TaintGrammar().getOpt(), new TaintWithContextRelationManager(), true);
 		run(new ShordRelationReader(), new ImplicitFlowGrammar().getOpt(), new ImplicitFlowRelationManager(), true);
-		run(new ShordRelationReader(), new MissingRefRefTaintGrammar().getOpt(), new MissingRefRefRelationManager(), true);
+		run(new ShordRelationReader(), new MissingRefRefTaintGrammar().getOpt(), new MissingRefRefTaintRelationManager(), true);
 		run(new ShordRelationReader(), new MissingRefRefImplicitFlowGrammar().getOpt(), new MissingRefRefImplicitFlowRelationManager(), true);
 		
 		run(new ShordRelationReader(), new AliasModelsGrammar().getOpt(), new AliasModelsRelationManager(), false);
