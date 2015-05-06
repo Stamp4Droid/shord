596116047c1dfebd1a08ddae0a79ebd5ba4ebfec
saswat Anand <saswat@kuhu2.stanford.edu>
2015-05-06 00:34:50 -0700

    fix model

--- models/src/java/io/ByteArrayOutputStream.java
+++ models/src/java/io/ByteArrayOutputStream.java
@@ -1,5 +1,13 @@
 class ByteArrayOutputStream
 {
+	public ByteArrayOutputStream() {
+        this.buf = new byte[1];
+    }
+
+    public ByteArrayOutputStream(int size) {
+        this.buf = new byte[size];
+    }
+
 	@STAMP(flows = {@Flow(from="this",to="@return")})
 	public synchronized byte[] toByteArray() { 
 		return new byte[1];
@@ -22,17 +30,20 @@ class ByteArrayOutputStream
 
 	@STAMP(flows = {@Flow(from="buffer",to="this")})
 	public synchronized void write(byte[] buffer, int offset, int len) {
+		this.buf[0] = buffer[0];
 	}
 
 	@STAMP(flows={@Flow(from="buffer",to="this")})
 	public  void write(byte[] buffer) throws java.io.IOException { 
+		this.buf[0] = buffer[0];
 	}
 
 	@STAMP(flows = {@Flow(from="oneByte",to="this")})
 	public synchronized void write(int oneByte) {
+		this.buf[0] = (byte) oneByte;
 	}
 
 	public synchronized void writeTo(java.io.OutputStream out) throws java.io.IOException {
-		out.write(toByteArray());
+		out.write(this.buf);
 	}
 }
