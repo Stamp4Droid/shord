b2f77c02f3e07d0c8a5523d9937afe1cd67eb960
Osbert Bastani <obastani@gmail.com>
2015-06-05 16:28:47 -0700

    removed points-to grammar from alias models inference, added active version of analysis, optimized alias models grammar a bit

--- main/src/stamp/missingmodels/util/cflsolver/grammars/AliasModelsGrammar.java
+++ main/src/stamp/missingmodels/util/cflsolver/grammars/AliasModelsGrammar.java
@@ -1,48 +1,136 @@
 package stamp.missingmodels.util.cflsolver.grammars;
 
-public class AliasModelsGrammar extends PointsToGrammar {
+import java.util.List;
+
+import stamp.missingmodels.util.cflsolver.core.ContextFreeGrammar;
+
+public class AliasModelsGrammar extends ContextFreeGrammar {
+	public static class TaintAliasModelsGrammar extends UnionGrammar {
+		public TaintAliasModelsGrammar() {
+			super(new AliasModelsGrammar(), new TaintGrammar());
+		}
+	}
+	
+	public static class ActiveAliasModelsGrammar extends ContextFreeGrammar {
+		public ActiveAliasModelsGrammar() {
+			AliasModelsGrammar g = new AliasModelsGrammar();
+			for(List<UnaryProduction> ups : g.unaryProductionsByTarget) {
+				for(UnaryProduction up : ups) {
+					this.addUnaryProduction(up.target.symbol, up.input.symbol, up.isInputBackwards, up.ignoreFields, up.ignoreContexts);
+				}
+			}
+			for(List<BinaryProduction> bps : g.binaryProductionsByTarget) {
+				for(BinaryProduction bp : bps) {
+					if(bp.firstInput.symbol.equals("Flow") && bp.secondInput.symbol.equals("Bassign")) {
+						continue;
+					}
+					if(bp.firstInput.symbol.equals("FlowPost") && bp.secondInput.symbol.equals("assignE")) {
+						continue;
+					}
+					this.addBinaryProduction(bp.target.symbol, bp.firstInput.symbol, bp.secondInput.symbol, bp.isFirstInputBackwards, bp.isSecondInputBackwards, bp.ignoreFields, bp.ignoreContexts);
+				}
+			}
+			for(List<AuxProduction> aps : g.auxProductionsByTarget) {
+				for(AuxProduction ap : aps) {
+					this.addAuxProduction(ap.target.symbol, ap.input.symbol, ap.auxInput.symbol, ap.isAuxInputFirst, ap.isInputBackwards, ap.isAuxInputBackwards, ap.ignoreFields, ap.ignoreContexts);
+				}
+			}
+			this.addProduction("FlowPre", new String[]{"activeObject", "Flow", "Bassign"});
+			
+			this.addProduction("FlowPre", new String[]{"Flow", "Bassign", "ActiveMethod"});
+			this.addAuxProduction("activeObject", "objectSelf", "FlowPre", false);
+			
+			this.addAuxProduction("ActiveMethod", "methodSelf", "FlowPre", false);
+			this.addAuxProduction("ActiveMethod", "methodSelf", "FlowPrePost", false);
+			this.addBinaryProduction("FlowPost", "ActiveMethod", "assignE");
+		}
+	}
+	
+	public static class TaintActiveAliasModelsGrammar extends UnionGrammar {
+		public TaintActiveAliasModelsGrammar() {
+			super(new ActiveAliasModelsGrammar(), new TaintGrammar());
+		}
+	}
+	
 	public AliasModelsGrammar() {
 		// (1) A_v rules
-		this.addBinaryProduction("FlowPre", "Flow", "Bassign");
-		this.addUnaryProduction("FlowPre", "FlowFieldAnyPre");
+		
+		// Flow -> Flow Assign
+		this.addBinaryProduction("FlowPre", "Flow", "Bassign"); // ***
+		
+		// Flow -> Flow Store[f] FlowBar Flow Load[f]
+		this.addProduction("FlowPre", new String[]{"Flow", "store", "FlowPost"}, new boolean[]{false, false, true}, true); // ***
+		
+		// Flow ->  FlowField Flow Load[f]
 		this.addBinaryProduction("FlowPre", "FlowField", "FlowPre", true);
-		this.addUnaryProduction("FlowFieldAnyPre", "FlowPre");
-		this.addProduction("FlowFieldAnyPre", new String[]{"Flow", "store", "FlowPost"}, new boolean[]{false, false, true}, true);
 		
 		// (2) A^v rules
-		this.addUnaryProduction("FlowPost", "assignE");
+		
+		// Flow -> Flow Assign
+		this.addUnaryProduction("FlowPost", "assignE");	// ***
 		this.addBinaryProduction("FlowPost", "FlowPost", "assign");
 		this.addBinaryProduction("FlowPost", "FlowPost", "param");
 		this.addBinaryProduction("FlowPost", "FlowPost", "return");
+		
+		// Flow -> Flow Store[f] FlowBar Flow Load[f]
 		this.addBinaryProduction("FlowPost", "FlowPost", "load", true);
+		
+		// FlowField -> Flow Store[f] FlowBar
+		this.addProduction("FlowFieldPost", new String[]{"FlowPost", "store", "Flow"}, new boolean[]{false, false, true});
+		this.addUnaryProduction("FlowFieldAnyPost", "FlowPre", true);
+		
+		// Flow -> FlowField Flow Load[f]
 		this.addProduction("FlowPost", new String[]{"FlowFieldPost", "Flow", "load"});
 		this.addProduction("FlowPost", new String[]{"FlowFieldAnyPost", "Flow", "load"}, true);
-		this.addUnaryProduction("FlowFieldAnyPost", "FlowPre", true);
-		this.addProduction("FlowFieldPost", new String[]{"FlowPost", "store", "Flow"}, new boolean[]{false, false, true});
 		
 		// (3) A^w_v rules
-		this.addBinaryProduction("FlowPrePost", "FlowPost", "Bassign");
-		this.addUnaryProduction("FlowPrePost", "FlowFieldAnyPrePost");
+		
+		// Flow -> Flow Assign
+		this.addBinaryProduction("FlowPrePost", "FlowPost", "Bassign"); // ***
+		
+		// Flow -> FlowField Flow Load[f]
 		this.addBinaryProduction("FlowPrePost", "FlowFieldPost", "FlowPre", true);
 		this.addBinaryProduction("FlowPrePost", "FlowFieldAnyPost", "FlowPre");
-		this.addUnaryProduction("FlowFieldAnyPrePost", "FlowPrePost");
-		this.addUnaryProduction("FlowFieldAnyPrePost", "FlowPrePost", true); // "true" is for backwards input
-		this.addProduction("FlowFieldAnyPrePost", new String[]{"FlowPost", "store", "FlowPost"}, new boolean[]{false, false, true}, true);
 		
-		// (4) A rules
-		this.addBinaryProduction("Flow", "FlowPre", "assignE");
-		this.addProduction("Flow", new String[]{"FlowFieldAnyPre", "FlowPost", "load"}, true);
-		this.addBinaryProduction("FlowFieldAny", "FlowPre", "FlowPre", false, true);
-		
-		this.addBinaryProduction("FlowFieldAnyPre", "FlowPre", "FlowPrePost", false, true);
-		
-		this.addBinaryProduction("FlowFieldAnyPrePost", "FlowPrePost", "FlowPrePost", false, true);
-		
-		this.addBinaryProduction("FlowPost", "FlowPrePost", "assignE");
-		this.addProduction("FlowPost", new String[]{"FlowFieldAnyPrePost", "FlowPost", "load"}, true);
+		// Flow -> Flow Store[f] FlowBar Flow Load[f]
+		this.addUnaryProduction("FlowPrePost", "FlowPrePost", true);
+		this.addProduction("FlowPrePost", new String[]{"FlowPost", "store", "FlowPost"}, new boolean[]{false, false, true}, true);
+
+		// FLowField -> Flow Store[f] FlowBar
 		this.addBinaryProduction("FlowFieldAnyPost", "FlowPrePost", "FlowPre", false, true);
 		
+		// (4) Stitching rules
+		
+		// (4i) A rules
+		
+		// Flow -> Flow Assign
+		this.addBinaryProduction("FlowNew", "FlowPre", "assignE");
+		
+		// Flow -> Flow Store[f] FlowBar Flow Load[f]
+		this.addProduction("FlowNew", new String[]{"FlowPre", "FlowPost", "load"}, true);
+		
+		// FlowField -> Flow Store[f] FlowBar
+		this.addBinaryProduction("FlowFieldAny", "FlowPre", "FlowPre", false, true);
+		
+		// (4ii) A_v rules
+		
+		// Flow -> Flow Store[f] FlowBar Flow Load[f]
+		this.addBinaryProduction("FlowPre", "FlowPre", "FlowPrePost", false, true);
+		
+		// (4iii) A^v rules
+		
+		// Flow -> Flow Assign
+		this.addBinaryProduction("FlowPost", "FlowPrePost", "assignE");
+		
+		// Flow -> Flow Store[f] FlowBar Flow Load[f]
+		this.addProduction("FlowPost", new String[]{"FlowPrePost", "FlowPost", "load"}, true);
+		
+		// (4iv) A^w_v rules
+		
+		// Flow -> Flow Store[f] FlowBar Flow Load[f]
+		this.addBinaryProduction("FlowPrePost", "FlowPrePost", "FlowPrePost", false, true);
+		
 		// semantics for FlowFieldAny
-		this.addProduction("Flow", new String[]{"FlowFieldAny", "Flow", "load"}, true);
+		this.addProduction("FlowNew", new String[]{"FlowFieldAny", "Flow", "load"}, true);
 	}
 }
--- main/src/stamp/missingmodels/util/cflsolver/relation/AliasModelsRelationManager.java
+++ main/src/stamp/missingmodels/util/cflsolver/relation/AliasModelsRelationManager.java
@@ -1,8 +1,29 @@
 package stamp.missingmodels.util.cflsolver.relation;
 
 public class AliasModelsRelationManager extends PointsToRelationManager {
+	public static class TaintAliasModelsRelationManager extends UnionRelationManager {	
+		public TaintAliasModelsRelationManager() {
+			super(new AliasModelsRelationManager(), new TaintRelationManager());
+		}
+	}
+	
+	public static class ActiveAliasModelsRelationManager extends AliasModelsRelationManager {
+		public ActiveAliasModelsRelationManager() {
+			this.add(new IndexRelation("StubParam", "M", 0, "M", 0, "methodSelf", null, (short)0));
+			this.add(new IndexRelation("AllocNew", "H", 1, "H", 1, "objectSelf", null, (short)0));
+		}
+	}
+	
+	public static class TaintActiveAliasModelsRelationManager extends UnionRelationManager {
+		public TaintActiveAliasModelsRelationManager() {
+			super(new ActiveAliasModelsRelationManager(), new TaintRelationManager());
+		}
+	}
+	
 	public AliasModelsRelationManager() {
 		this.add(new IndexRelation("StubParam", "V", 1, "M", 0, "Bassign", null, (short)1));
 		this.add(new IndexRelation("StubReturn", "M", 0, "V", 1, "assignE", null, (short)1));
-	}	
+		this.add(new IndexRelation("pt", "C", 2, "V", 1, "Flow", null, (short)0));
+		this.add(new IndexRelation("fpt", "C", 2, "C", 0, "FlowField", 1, (short)0));
+	}
 }
