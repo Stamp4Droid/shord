547d15bb87720104e35fa54cf5697413288b87b3
Osbert Bastani <obastani@gmail.com>
2015-06-08 22:45:46 -0700

    added model for Object.<init>

--- models/src/java/lang/Object.java
+++ models/src/java/lang/Object.java
@@ -2,6 +2,8 @@ class Object
 {
 	protected  java.lang.Object clone() throws java.lang.CloneNotSupportedException { return this; }
 
+    public Object() {}
+
 	@STAMP(flows = {@Flow(from="this",to="@return")})
 	public  java.lang.String toString() 
 	{ 
