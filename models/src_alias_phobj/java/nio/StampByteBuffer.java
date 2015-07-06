55a35326e9bd4e4f3a10ba1841ed94a50a0e1b57
Osbert Bastani <obastani@gmail.com>
2015-07-06 08:06:49 +0100

    fixed bugs in alias_phobj models

--- models/src_alias_phobj/java/nio/StampByteBuffer.java
+++ models/src_alias_phobj/java/nio/StampByteBuffer.java
@@ -5,10 +5,10 @@ import edu.stanford.stamp.annotation.Flow;
 
 public class StampByteBuffer extends ByteBuffer 
 {
-    /*
+
 	@STAMP(flows = {@Flow(from="this",to="@return")})
     public java.nio.CharBuffer asCharBuffer(){ return new StampCharBuffer(); }
-    */
+
 
     public java.nio.DoubleBuffer asDoubleBuffer() {
         throw new RuntimeException("Stub!");
