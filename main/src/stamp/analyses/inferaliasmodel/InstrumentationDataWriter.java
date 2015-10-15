1dcc3f47f168bf4e03f54c36ff77ef7ffd07f7d5
Osbert Bastani <obastani@gmail.com>
2015-10-14 19:26:17 -0700

    added instrumentation analysis for points-to cuts

---
+++ main/src/stamp/analyses/inferaliasmodel/InstrumentationDataWriter.java
@@ -0,0 +1,136 @@
+package stamp.analyses.inferaliasmodel;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import shord.program.Program;
+import shord.project.analyses.JavaAnalysis;
+import soot.Local;
+import soot.RefLikeType;
+import soot.SootClass;
+import soot.SootMethod;
+import soot.Unit;
+import soot.Value;
+import soot.jimple.AnyNewExpr;
+import soot.jimple.DefinitionStmt;
+import soot.jimple.NewExpr;
+import soot.jimple.Stmt;
+import soot.tagkit.BytecodeOffsetTag;
+import soot.tagkit.Tag;
+import stamp.analyses.inferaliasmodel.MatchAllocToInitAnalysis2.MultivalueMap;
+import chord.project.Chord;
+
+@Chord(name = "instrument")
+public class InstrumentationDataWriter extends JavaAnalysis {
+	private static class EventWriter {
+		private final PrintWriter writer;
+		private int eventId = 0;
+		private EventWriter(PrintWriter writer) {
+			this.writer = writer;
+		}
+		private void writeMethodCallArg(String methodSignature, int bytecodeOffset, int argIndex) {
+			this.writer.println("METHCALLARG " + methodSignature + " " + bytecodeOffset + " " + argIndex + " " + this.eventId++);
+		}
+		private void writeMethodParam(String methodSignature, int argIndex) {
+			this.writer.println("METHPARAM " + methodSignature + " " + argIndex + " " + this.eventId++);
+		}
+		private void close() {
+			this.writer.close();
+		}
+	}
+	
+	public static void write() {
+		try {
+			writeHelper();
+		} catch(IOException e) {
+			e.printStackTrace();
+			throw new RuntimeException("Failed to write instrumentation!");
+		}
+	}
+	
+	private static void writeHelper() throws IOException {
+		File outDir = new File(System.getProperty("stamp.out.dir"), "inferaliasmodel");
+		outDir.mkdirs();
+		EventWriter eventWriter = new EventWriter(new PrintWriter(new BufferedWriter(new FileWriter(new File(outDir, "instrinfo.txt")))));
+		for(SootClass klass : Program.g().getClasses()) {
+			for(SootMethod method : klass.getMethods()) {
+				if(method.isConcrete()) {
+					processMethod(method, eventWriter);
+				}
+			}
+		}
+		eventWriter.close();
+	}
+	
+	private static void processMethod(SootMethod method, EventWriter writer) {
+		String methodSignature = bytecodeSignature(method);
+		MultivalueMap<Stmt,Stmt> matchAllocToInit = MatchAllocToInitAnalysis2.getMatchAllocToInit(method.retrieveActiveBody());
+		
+		// Handle params
+		if(method.isStatic()) {
+			writer.writeMethodParam(methodSignature, 0);
+		}
+		int offset = method.isStatic() ? 0 : 1;
+		for(int i=0; i<method.getParameterCount(); i++) {
+			if(method.getParameterType(i) instanceof RefLikeType) {
+				writer.writeMethodParam(methodSignature, i + offset);
+			}
+		}
+		
+		// Handle units
+		for(Unit unit : method.retrieveActiveBody().getUnits()) {
+			Stmt stmt = (Stmt)unit;
+			
+			// Handle allocation statement
+			if(stmt instanceof DefinitionStmt) {
+				Value leftOp = ((DefinitionStmt)stmt).getLeftOp();
+				Value rightOp = ((DefinitionStmt)stmt).getRightOp();
+				if(leftOp instanceof Local && rightOp instanceof NewExpr) {
+					for(Stmt initInvkStmt : matchAllocToInit.get(stmt)) {
+						int bytecodeOffset = bytecodeOffset(initInvkStmt);
+						if(bytecodeOffset >= 0) {
+							writer.writeMethodCallArg(methodSignature, bytecodeOffset, 0);
+						} else {
+							System.out.println("bytecode offset unavailable: " + method.getSignature());
+						}
+					}
+				} else if(leftOp instanceof Local && rightOp instanceof AnyNewExpr) {
+					// TODO: handle array allocation sites
+				}
+			}
+			
+			// Handle invocation statement
+			if(stmt.containsInvokeExpr() && (stmt instanceof DefinitionStmt)) {
+				int bytecodeOffset = bytecodeOffset(stmt);
+				if(bytecodeOffset >= 0) {
+					writer.writeMethodCallArg(methodSignature, bytecodeOffset, -1);
+				} else {
+					System.out.println("bytecode offset unavailable: "+method.getSignature());
+				}
+			}
+		}
+	}
+	
+	
+	@Override
+	public void run() {
+		write();
+	}
+	
+	private static int bytecodeOffset(Stmt stmt) {
+		for(Tag tag : stmt.getTags()) {
+			if(tag instanceof BytecodeOffsetTag) {
+				return ((BytecodeOffsetTag)tag).getBytecodeOffset();
+			}
+		}
+		return -1;
+	}
+
+	private static String bytecodeSignature(SootMethod method) {
+		String sig = method.getBytecodeSignature();
+		return sig.substring(sig.indexOf(' ')+1, sig.length()-1) + "@L" + method.getDeclaringClass().getName().replace('.', '/') + ";";
+	}
+}
---
+++ main/src/stamp/analyses/inferaliasmodel/MatchAllocToInitAnalysis2.java
@@ -0,0 +1,91 @@
+package stamp.analyses.inferaliasmodel;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Set;
+
+import soot.Body;
+import soot.Local;
+import soot.Unit;
+import soot.Value;
+import soot.jimple.DefinitionStmt;
+import soot.jimple.InstanceInvokeExpr;
+import soot.jimple.InvokeExpr;
+import soot.jimple.NewExpr;
+import soot.jimple.Stmt;
+import soot.toolkits.graph.ExceptionalUnitGraph;
+import soot.toolkits.scalar.SimpleLocalDefs;
+import chord.util.tuple.object.Pair;
+
+public class MatchAllocToInitAnalysis2 {
+	public static class MultivalueMap<K,V> extends HashMap<K,List<V>> {
+		private static final long serialVersionUID = -6390444829513305915L;
+
+		public void add(K k, V v) {
+			ensure(k).add(v);
+		}
+		
+		public List<V> ensure(K k) {
+			List<V> vSet = super.get(k);
+			if(vSet == null) {
+				super.put(k, vSet = new ArrayList<V>());
+			}
+			return vSet;
+		}
+
+		@Override
+		public List<V> get(Object k) {
+			List<V> vSet = super.get(k);
+			return Collections.unmodifiableList(vSet == null ? new ArrayList<V>() : vSet);
+		}
+	}
+	
+	public static MultivalueMap<Stmt,Stmt> getMatchAllocToInit(Body body) {
+		MultivalueMap<Stmt,Stmt> newStmtToInvokeInitStmts = new MultivalueMap<Stmt,Stmt>();
+		SimpleLocalDefs ld = new SimpleLocalDefs(new ExceptionalUnitGraph(body));
+		for(Unit unit : body.getUnits()) {
+			Stmt initInvokeStmt = (Stmt)unit;
+			if(!initInvokeStmt.containsInvokeExpr()) {
+				continue;
+			}
+			InvokeExpr ie = initInvokeStmt.getInvokeExpr();
+			if(!ie.getMethod().getName().equals("<init>")) {
+				continue;
+			}
+			Local rcvr = (Local)((InstanceInvokeExpr)ie).getBase();
+			List<Pair<Local,Stmt>> workList = new LinkedList<Pair<Local,Stmt>>();
+			workList.add(new Pair<Local,Stmt>(rcvr, initInvokeStmt));
+			Set<Pair<Local,Stmt>> visited = new HashSet<Pair<Local,Stmt>>();
+			while(!workList.isEmpty()) {
+				Pair<Local,Stmt> p = workList.remove(0);
+				if(visited.contains(p)) {
+					continue;
+				}
+				visited.add(p);
+				
+				Local local = p.val0;
+				Stmt useStmt = p.val1;
+				
+				for(Unit stmt : ld.getDefsOfAt(local, useStmt)) {
+					if(stmt instanceof DefinitionStmt){
+						DefinitionStmt ds = (DefinitionStmt) stmt;
+						Value leftOp = ds.getLeftOp();
+						Value rightOp = ds.getRightOp();
+						assert local.equals(leftOp);
+						if(rightOp instanceof NewExpr) {
+							newStmtToInvokeInitStmts.add(ds, initInvokeStmt);
+						} else if(rightOp instanceof Local) {
+							workList.add(new Pair<Local,Stmt>((Local)rightOp, ds));
+						}
+					}				
+				}
+			}
+		}
+		return newStmtToInvokeInitStmts;
+	}
+	
+}
