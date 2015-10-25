ee172f1efbc6e85bf314aaf45a6c01373fd3c86c
Osbert Bastani <obastani@gmail.com>
2015-10-25 16:40:34 -0400

    updated multiple leaks android manifest added preliminary monitor map computations updated stamp config to run more analyses

---
+++ main/src/stamp/analyses/inferaliasmodel/MonitorMapUtils.java
@@ -0,0 +1,188 @@
+package stamp.analyses.inferaliasmodel;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import shord.analyses.AllocNode;
+import shord.analyses.CastVarNode;
+import shord.analyses.DomM;
+import shord.analyses.DomV;
+import shord.analyses.LocalVarNode;
+import shord.analyses.ParamVarNode;
+import shord.analyses.RetVarNode;
+import shord.analyses.StringConstantVarNode;
+import shord.analyses.ThisVarNode;
+import shord.analyses.VarNode;
+import shord.project.ClassicProject;
+import soot.Local;
+import soot.SootMethod;
+import soot.Unit;
+import soot.Value;
+import soot.ValueBox;
+import soot.jimple.AssignStmt;
+import soot.jimple.CastExpr;
+import soot.jimple.Stmt;
+import soot.jimple.StringConstant;
+import stamp.analyses.inferaliasmodel.InstrumentationDataWriter.Monitor;
+import stamp.missingmodels.util.Util.MultivalueMap;
+import stamp.missingmodels.util.Util.Pair;
+
+public class MonitorMapUtils {
+	public static class VariableMap {
+		private final MultivalueMap<Local,Stmt> localToDef = new MultivalueMap<Local,Stmt>();
+		private final MultivalueMap<Local,Stmt> localToUse = new MultivalueMap<Local,Stmt>();
+		private final Map<Stmt,SootMethod> stmtToMethod = new HashMap<Stmt,SootMethod>();
+		private final Map<Local,SootMethod> localToMethod = new HashMap<Local,SootMethod>();
+		private final Map<CastExpr,Stmt> castToStmt = new HashMap<CastExpr,Stmt>();
+		private final MultivalueMap<Pair<SootMethod,String>,Stmt> strToStmt = new MultivalueMap<Pair<SootMethod,String>,Stmt>();
+		
+		public Set<Stmt> getDefs(Local local) {
+			return this.localToDef.get(local);
+		}
+		
+		public Set<Stmt> getUses(Local local) {
+			return this.localToUse.get(local);
+		}
+		
+		public SootMethod getMethod(Stmt stmt) {
+			return this.stmtToMethod.get(stmt);
+		}
+		
+		public SootMethod getMethod(Local local) {
+			return this.localToMethod.get(local);
+		}
+		
+		public Stmt getStmt(CastExpr expr) {
+			return this.castToStmt.get(expr);
+		}
+		
+		public Set<Stmt> getStmts(SootMethod method, String str) {
+			return this.strToStmt.get(new Pair<SootMethod,String>(method, str));
+		}
+		
+		public VariableMap() {
+			Iterator<SootMethod> methods = ((DomM)ClassicProject.g().getTrgt("M")).iterator();
+			while(methods.hasNext()) {
+				process(methods.next());
+			}
+		}
+		
+		private void process(SootMethod method) {
+			if(!method.hasActiveBody()) {
+				return;
+			}
+			for(Unit unit : method.getActiveBody().getUnits()) {
+				Stmt stmt = (Stmt)unit;
+				
+				// Stmt methods
+				this.stmtToMethod.put(stmt, method);
+				
+				// Def boxes
+				for(ValueBox def : stmt.getDefBoxes()) {
+					if(def.getValue() instanceof Local) {
+						//System.out.println("DEF: " + def.getValue() + " ## " + stmt);
+						this.localToDef.add((Local)def.getValue(), stmt);
+						add((Local)def.getValue(), method);
+					} else {
+						//System.out.println("VALUE TYPE: " + def.getValue().getClass().getName());
+					}
+				}
+				
+				// Use boxes
+				for(ValueBox use : stmt.getUseBoxes()) {
+					if(use.getValue() instanceof Local) {
+						//System.out.println("USE: " + use.getValue() + " ## " + stmt);
+						this.localToUse.add((Local)use.getValue(), stmt);
+						add((Local)use.getValue(), method);
+					} else {
+						//System.out.println("VALUE TYPE: " + use.getValue().getClass().getName());
+					}
+				}
+				
+				// Cast expressions
+				if(stmt instanceof AssignStmt) {
+					Value rightOp = ((AssignStmt)stmt).getRightOp();
+					if(rightOp instanceof CastExpr) {
+						this.castToStmt.put((CastExpr)rightOp, stmt);
+					}
+				}
+				
+				// String constants
+				for(ValueBox use : stmt.getUseBoxes()) {
+					if(use.getValue() instanceof StringConstant) {
+						this.strToStmt.add(new Pair<SootMethod,String>(method, ((StringConstant)use.getValue()).value), stmt);
+					}
+				}
+			}
+		}
+	
+		private void add(Local local, SootMethod method) {
+			if(this.localToMethod.containsKey(local)) {
+				if(!this.localToMethod.get(local).equals(method)) {
+					throw new RuntimeException("Mismatched methods!");
+				}
+			}
+			this.localToMethod.put(local, method);
+		}
+	}
+	
+	public static class MonitorMap {
+		private final MultivalueMap<VarNode,Monitor> varToMonitor = new MultivalueMap<VarNode,Monitor>();
+		private final MultivalueMap<AllocNode,Monitor> allocToMonitor = new MultivalueMap<AllocNode,Monitor>();
+		
+		public MonitorMap() {
+			Iterator<VarNode> varIter = ((DomV)ClassicProject.g().getTrgt("V")).iterator();
+			List<VarNode> vars = new ArrayList<VarNode>();
+			while(varIter.hasNext()) {
+				vars.add(varIter.next());
+			}
+			processVars(vars);
+		}
+		
+		private void processVars(Iterable<VarNode> vars) {
+			VariableMap varMap = new VariableMap();
+			for(VarNode var : vars) {
+				//System.out.println("VAR: " + var);
+				if(var instanceof CastVarNode) {
+					this.processDefinition(varMap.getStmt(((CastVarNode)var).castExpr));
+				} else if(var instanceof LocalVarNode) {
+					for(Stmt stmt : varMap.getDefs(((LocalVarNode)var).local)) {
+						this.processDefinition(stmt);
+					}
+				} else if(var instanceof ParamVarNode) {
+					ParamVarNode paramVar = (ParamVarNode)var;
+					this.processMethodParam(paramVar.method, paramVar.index);
+				} else if(var instanceof RetVarNode) {
+					this.processMethodReturn(((RetVarNode)var).method);
+				} else if(var instanceof StringConstantVarNode) {
+					StringConstantVarNode constVar = (StringConstantVarNode)var;
+					for(Stmt stmt : varMap.getStmts(constVar.method, constVar.sc)) {
+						this.processDefinition(stmt);
+					}
+				} else if(var instanceof ThisVarNode) {
+					this.processMethodThis(((ThisVarNode)var).method);
+				}
+			}
+		}
+		
+		private void processDefinition(Stmt stmt) {
+			
+		}
+		
+		private void processMethodParam(SootMethod method, int index) {
+			
+		}
+		
+		private void processMethodThis(SootMethod method) {
+			
+		}
+		
+		private void processMethodReturn(SootMethod method) {
+			
+		}		
+	}	
+}
