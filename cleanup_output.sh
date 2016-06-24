a298f9fd3b817f883e177a4860378e8a05ac04d0
Osbert Bastani <obastani@stanford.edu>
2016-06-24 16:36:35 -0700

    added cleanup script

---
+++ cleanup_output.sh
@@ -0,0 +1,8 @@
+rm -rf stamp_output/*/hierarchy.json
+rm -rf stamp_output/*/jimple
+rm -rf stamp_output/*/reports.txt
+rm -rf stamp_output/*/srcmap
+rm -rf stamp_output/*/stamp_annotations.txt
+rm -rf stamp_output/*/stamp-driver
+rm -rf stamp_output/*/stamp.properties
+rm -rf stamp_output/*/widgets.txt
