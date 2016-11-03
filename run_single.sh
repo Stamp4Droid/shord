ac67c325bcbdd2c26f1c2dbfae8035d0a5bb9261
Osbert Bastani <obastani@cs.stanford.edu>
2016-11-03 15:51:41 -0700

    bug fix

--- run_single.sh
+++ run_single.sh
@@ -2,5 +2,5 @@ echo java -jar -Dchord.work.dir=../pjbench/dacapo/benchmarks/$3 -Dchord.model.pa
 java -jar -Dchord.work.dir=../pjbench/dacapo/benchmarks/$3 -Dchord.model.path=$1 shord.jar
 echo mkdir -p results
 mkdir -p results
-echo mv ../pjbench/dacapo/benchmarks/chord_output/log.txt results/log_$3_$2.txt
-mv ../pjbench/dacapo/benchmarks/chord_output/log.txt results/log_$3_$2.txt
+echo mv ../pjbench/dacapo/benchmarks/$3/chord_output/log.txt results/log_$3_$2.txt
+mv ../pjbench/dacapo/benchmarks/$3/chord_output/log.txt results/log_$3_$2.txt
