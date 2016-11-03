17c0db9fba7fa97e1093464f4408ae6d3534afc7
Osbert Bastani <obastani@cs.stanford.edu>
2016-11-03 15:34:35 -0700

    minor

--- run_single.sh
+++ run_single.sh
@@ -1,3 +1,3 @@
-mkdir -p results/
 java -jar -Dchord.work.dir=../pjbench/dacapo/benchmarks/$2 -Dchord.model.path=$1 shord.jar
+mkdir -p results/
 mv ../pjbench/dacapo/benchmarks/chord_output/log.txt results/log_$2_$3.txt
