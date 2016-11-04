ed65213236d404bc01ccea79a77a6def49de1898
Osbert Bastani <obastani@cs.stanford.edu>
2016-11-04 00:17:37 -0700

    fixed bug

--- main/src/stamp/analyses/inferaliasmodel/AliasModelsStubOnly.java
+++ main/src/stamp/analyses/inferaliasmodel/AliasModelsStubOnly.java
@@ -188,7 +188,7 @@ public class AliasModelsStubOnly {
 	public static Set<SootMethod> getReachableM() {
 		if(reachableM == null) {
 			reachableM = new HashSet<SootMethod>();
-			ProgramRel relReachableM = (ProgramRel)ClassicProject.g().getTrgt("reachableM");
+			ProgramRel relReachableM = (ProgramRel)ClassicProject.g().getTrgt("ci_reachableM");
 			relReachableM.load();
 			for(Object m : relReachableM.getAry1ValTuples()) {
 				reachableM.add((SootMethod)m);
